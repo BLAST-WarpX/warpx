2d78c278da996ff94505db14dd3db65070601571
David Grote <grote1@llnl.gov>
2024-11-04 15:26:33 -0800

    Fix Copy in WarpXSolverVec.cpp

--- Source/FieldSolver/ImplicitSolvers/WarpXSolverVec.cpp
+++ Source/FieldSolver/ImplicitSolvers/WarpXSolverVec.cpp
@@ -123,14 +123,14 @@ void WarpXSolverVec::Copy ( FieldType  a_array_type,
 
     for (int lev = 0; lev < m_num_amr_levels; ++lev) {
         if (m_array_type != FieldType::None) {
-            const ablastr::fields::VectorField this_array = m_WarpX->m_fields.get_alldirs(m_vector_type_name, lev);
+            const ablastr::fields::VectorField this_array = m_WarpX->m_fields.get_alldirs(a_array_type, lev);
             for (int n = 0; n < 3; ++n) {
                 amrex::MultiFab::Copy( *m_array_vec[lev][n], *this_array[n], 0, 0, m_ncomp,
                                        amrex::IntVect::TheZeroVector() );
             }
         }
         if (m_scalar_type != FieldType::None) {
-            const amrex::MultiFab* this_mf = m_WarpX->m_fields.get(m_scalar_type_name,lev);
+            const amrex::MultiFab* this_mf = m_WarpX->m_fields.get(a_scalar_type,lev);
             amrex::MultiFab::Copy( *m_scalar_vec[lev], *this_mf, 0, 0, m_ncomp,
                                    amrex::IntVect::TheZeroVector() );
         }
