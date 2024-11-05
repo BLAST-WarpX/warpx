6b8c544771ece96a0d5e579b402a91d94e8869aa
Edoardo Zoni <ezoni@lbl.gov>
2024-11-04 16:25:51 -0800

    CI: checksum analysis with command line args

--- Examples/analysis_default_regression.py
+++ Examples/analysis_default_regression.py
@@ -1,25 +1,39 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
-import re
 import sys
 
 sys.path.insert(1, "../../../../warpx/Regression/Checksum/")
 from checksumAPI import evaluate_checksum
 
-test_name = os.path.split(os.getcwd())[1]
-output_file = sys.argv[1]
-
-# Run checksum regression test
-if re.search("single_precision", output_file):
-    evaluate_checksum(
-        test_name=test_name,
-        output_file=output_file,
-        rtol=2e-6,
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--path",
+        help="",
+        type=str,
     )
-else:
-    # using default relative tolerance
+    parser.add_argument(
+        "--format",
+        help="",
+        type=str,
+    )
+    parser.add_argument(
+        "--rtol",
+        help="",
+        type=float,
+        required=False,
+        default=1e-9,
+    )
+    # parse command line arguments
+    args = parser.parse_args()
+    # parse test name from test directory
+    test_name = os.path.split(os.getcwd())[1]
+    # compare checksums
     evaluate_checksum(
         test_name=test_name,
-        output_file=output_file,
+        output_file=args.path,
+        output_format=args.format,
+        rtol=args.rtol,
     )
