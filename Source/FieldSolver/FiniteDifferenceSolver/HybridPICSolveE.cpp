913760ca7b259f30e268c7645a98ec5d62fd87d3
S. Eric Clark <25495882+clarkse@users.noreply.github.com>
2024-11-07 12:32:29 -0800

    Redisabling filtering for RZ. Commenting out E field boundary.
    Signed-off-by: S. Eric Clark <25495882+clarkse@users.noreply.github.com>

--- Source/FieldSolver/FiniteDifferenceSolver/HybridPICSolveE.cpp
+++ Source/FieldSolver/FiniteDifferenceSolver/HybridPICSolveE.cpp
@@ -361,8 +361,8 @@ void FiniteDifferenceSolver::HybridPICSolveE (
         amrex::Abort(Utils::TextMsg::Err(
             "HybridSolveE: The hybrid-PIC electromagnetic solver algorithm must be used"));
     }
-    auto& warpx = WarpX::GetInstance();
-    warpx.ApplyEfieldBoundary(lev, PatchType::fine);
+    // auto& warpx = WarpX::GetInstance();
+    // warpx.ApplyEfieldBoundary(lev, PatchType::fine);
 }
 
 #ifdef WARPX_DIM_RZ
