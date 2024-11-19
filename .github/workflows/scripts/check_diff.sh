f8d50363f1b75997be15ff42584e6b2ce399c5fb
Edoardo Zoni <ezoni@lbl.gov>
2024-11-18 21:14:55 -0800

    Debugging

--- .github/workflows/scripts/check_diff.sh
+++ .github/workflows/scripts/check_diff.sh
@@ -3,6 +3,7 @@
 set -o nounset
 set -o errexit
 set -o pipefail
+set -o xtrace
 
 # Parse command line arguments
 head_ref=${1}
@@ -12,8 +13,11 @@ clone_url=${3}
 # Set paths to ignore
 paths_ignore="^(Docs|\.github)/|\.azure-pipelines\.yml$"
 
-# Add forked repository as remote
-git remote add fork ${clone_url}
+# Check if remote repository already exists
+if ! git remote --get-url fork > /dev/null 2>&1; then
+  # Add forked repository as remote
+  git remote add fork ${clone_url}
+fi
 
 # Fetch base branch from main repository
 git fetch origin ${base_ref}
@@ -24,12 +28,17 @@ git fetch fork ${head_ref}
 # Save output of git diff to inspect files changed
 git diff --name-only --diff-filter=ACMRTUXB origin/${base_ref}..fork/${head_ref} > check_diff.txt
 
-# Set skip variable after inspecting files changed
-skip=$(grep -v -E "${paths_ignore}" check_diff.txt)
-
-# Set an environment variable based on the output
-if [ -z "$skip" ]; then
-  echo "SKIP_CHECKS=true" >> $GITHUB_ENV
+# Check if check_diff.txt is not empty
+if [ -s check_diff.txt ]; then
+  # Set skip variable after inspecting files changed
+  skip=$(grep -v -E "${paths_ignore}" check_diff.txt)
+  # Set an environment variable based on the output
+  if [ -z "$skip" ]; then
+    echo "SKIP_CHECKS=true" >> $GITHUB_ENV
+  else
+    echo "SKIP_CHECKS=false" >> $GITHUB_ENV
+  fi
 else
-  echo "SKIP_CHECKS=false" >> $GITHUB_ENV
+  # If check_diff.txt is empty, set SKIP_CHECKS to true
+  echo "SKIP_CHECKS=true" >> $GITHUB_ENV
 fi
