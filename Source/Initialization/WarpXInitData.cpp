923a48ec7f3a746e6f2e3644b61365710a10402b
RevathiJambunathan <revanathan@gmail.com>
2024-12-17 12:24:39 -0800

    surface physic compilation and mapping of EB surface

--- Source/Initialization/WarpXInitData.cpp
+++ Source/Initialization/WarpXInitData.cpp
@@ -641,6 +641,8 @@ WarpX::InitData ()
 
     m_electrostatic_solver->InitData();
 
+    m_surface_physics->InitData();
+
     if (WarpX::electromagnetic_solver_id == ElectromagneticSolverAlgo::HybridPIC) {
         m_hybrid_pic_model->InitData(m_fields);
     }
