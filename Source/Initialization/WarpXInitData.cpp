ace399b6a3d044436563165a27bc93cb25b190b3
Remi Lehe <remi.lehe@normalesup.org>
2025-01-18 08:34:54 -0800

    Modify initialization in ECT solver

--- Source/Initialization/WarpXInitData.cpp
+++ Source/Initialization/WarpXInitData.cpp
@@ -1261,13 +1261,13 @@ void WarpX::InitializeEBGridData (int lev)
             ScaleAreas(face_areas_lev, CellSize(lev));
 
             if (WarpX::electromagnetic_solver_id == ElectromagneticSolverAlgo::ECT) {
+                // Compute additional quantities required for the ECT solver
+                MarkExtensionCells();
+                ComputeFaceExtensions();
                 // Mark on which grid points E should be updated
                 MarkUpdateECellsECT( m_eb_update_E[lev], edge_lengths_lev );
                 // Mark on which grid points B should be updated
                 MarkUpdateBCellsECT( m_eb_update_B[lev], face_areas_lev, edge_lengths_lev);
-                // Compute additional quantities required for the ECT solver
-                MarkExtensionCells();
-                ComputeFaceExtensions();
             } else {
                 // Mark on which grid points E should be updated (stair-case approximation)
                 MarkUpdateCellsStairCase(
