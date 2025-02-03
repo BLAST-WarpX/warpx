006f0f6225f2b305a25fd454c0174f23b6133ce1
Luca Fedeli <luca.fedeli@cea.fr>
2025-02-03 14:01:50 +0100

    bugfixing

--- Source/EmbeddedBoundary/EmbeddedBoundaryInit.cpp
+++ Source/EmbeddedBoundary/EmbeddedBoundaryInit.cpp
@@ -333,6 +333,95 @@ web::MarkUpdateBCellsECT (
 }
 
 void
+web::MarkExtensionCells ()
+{
+    using ablastr::fields::Direction;
+    using warpx::fields::FieldType;
+
+#ifndef WARPX_DIM_RZ
+    auto const &cell_size = CellSize(maxLevel());
+
+#if !defined(WARPX_DIM_3D) && !defined(WARPX_DIM_XZ)
+    WARPX_ABORT_WITH_MESSAGE("MarkExtensionCells only implemented in 2D and 3D");
+#endif
+
+    for (int idim = 0; idim < 3; ++idim) {
+#if defined(WARPX_DIM_XZ)
+        if (idim == 0 || idim == 2) {
+            m_flag_info_face[maxLevel()][idim]->setVal(0.);
+            m_flag_ext_face[maxLevel()][idim]->setVal(0.);
+            continue;
+        }
+#endif
+        for (amrex::MFIter mfi(*m_fields.get(FieldType::Bfield_fp, Direction{idim}, maxLevel())); mfi.isValid(); ++mfi) {
+            auto* face_areas_idim_max_lev =
+                m_fields.get(FieldType::face_areas, Direction{idim}, maxLevel());
+
+            const amrex::Box& box = mfi.tilebox(face_areas_idim_max_lev->ixType().toIntVect(),
+                                                face_areas_idim_max_lev->nGrowVect() );
+
+            auto const &S = face_areas_idim_max_lev->array(mfi);
+            auto const &flag_info_face = m_flag_info_face[maxLevel()][idim]->array(mfi);
+            auto const &flag_ext_face = m_flag_ext_face[maxLevel()][idim]->array(mfi);
+            const auto &lx = m_fields.get(FieldType::edge_lengths, Direction{0}, maxLevel())->array(mfi);
+            const auto &ly = m_fields.get(FieldType::edge_lengths, Direction{1}, maxLevel())->array(mfi);
+            const auto &lz = m_fields.get(FieldType::edge_lengths, Direction{2}, maxLevel())->array(mfi);
+            auto const &mod_areas_dim = m_fields.get(FieldType::area_mod, Direction{idim}, maxLevel())->array(mfi);
+
+            const amrex::Real dx = cell_size[0];
+            const amrex::Real dy = cell_size[1];
+            const amrex::Real dz = cell_size[2];
+
+            amrex::ParallelFor(box, [=] AMREX_GPU_DEVICE (int i, int j, int k) {
+                // Minimal area for this cell to be stable
+                mod_areas_dim(i, j, k) = S(i, j, k);
+                double S_stab;
+                if (idim == 0){
+                    S_stab = 0.5 * std::max({ly(i, j, k) * dz, ly(i, j, k + 1) * dz,
+                                                    lz(i, j, k) * dy, lz(i, j + 1, k) * dy});
+                }else if (idim == 1){
+#ifdef WARPX_DIM_XZ
+                    S_stab = 0.5 * std::max({lx(i, j, k) * dz, lx(i, j + 1, k) * dz,
+                                             lz(i, j, k) * dx, lz(i + 1, j, k) * dx});
+#elif defined(WARPX_DIM_3D)
+                    S_stab = 0.5 * std::max({lx(i, j, k) * dz, lx(i, j, k + 1) * dz,
+                                             lz(i, j, k) * dx, lz(i + 1, j, k) * dx});
+#endif
+                }else {
+                    S_stab = 0.5 * std::max({lx(i, j, k) * dy, lx(i, j + 1, k) * dy,
+                                             ly(i, j, k) * dx, ly(i + 1, j, k) * dx});
+                }
+
+                // Does this face need to be extended?
+                // The difference between flag_info_face and flag_ext_face is that:
+                //     - for every face flag_info_face contains a:
+                //          * 0 if the face needs to be extended
+                //          * 1 if the face is large enough to lend area to other faces
+                //          * 2 if the face is actually intruded by other face
+                //       Here we only take care of the first two cases. The entries corresponding
+                //       to the intruded faces are going to be set in the function ComputeFaceExtensions
+                //     - for every face flag_ext_face contains a:
+                //          * 1 if the face needs to be extended
+                //          * 0 otherwise
+                //       In the function ComputeFaceExtensions, after the cells are extended, the
+                //       corresponding entries in flag_ext_face are set to zero. This helps to keep
+                //       track of which cells could not be extended
+                flag_ext_face(i, j, k) = int(S(i, j, k) < S_stab && S(i, j, k) > 0);
+                if(flag_ext_face(i, j, k)){
+                    flag_info_face(i, j, k) = 0;
+                }
+                // Is this face available to lend area to other faces?
+                // The criterion is that the face has to be interior and not already unstable itself
+                if(int(S(i, j, k) > 0 && !flag_ext_face(i, j, k))) {
+                    flag_info_face(i, j, k) = 1;
+                }
+            });
+        }
+    }
+#endif
+}
+
+void
 web::ComputeEdgeLengths (
     ablastr::fields::VectorField& edge_lengths,
     const amrex::EBFArrayBoxFactory& eb_fact)
