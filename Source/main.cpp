9e93f3b62bbb1c8b86ddd2d31d70068dc410270e
Edoardo Zoni <ezoni@lbl.gov>
2025-02-19 13:00:13 -0800

    Change `ReferenceAlignment` to `Middle`

--- Source/main.cpp
+++ Source/main.cpp
@@ -24,7 +24,7 @@ main (int argc, char *argv[]) {
         auto timer = ablastr::utils::timer::Timer{};
         timer.record_start_time();
 
-        auto &warpx = WarpX::GetInstance();
+        auto & warpx = WarpX::GetInstance();
         warpx.InitData();
         warpx.Evolve();
         const auto is_warpx_verbose = warpx.Verbose();
