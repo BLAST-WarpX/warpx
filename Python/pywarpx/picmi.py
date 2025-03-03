aeaacbdfdde7e164f76df7ea02c65b4e21117d43
Luca Fedeli <luca.fedeli@for.unipi.it>
2025-03-03 12:53:34 +0100

    Update Python/pywarpx/picmi.py

--- Python/pywarpx/picmi.py
+++ Python/pywarpx/picmi.py
@@ -3033,7 +3033,7 @@ class Simulation(picmistandard.PICMI_Simulation):
 
         self.solver.solver_initialize_inputs()
 
-        # Initialize warpx.field_centering_no<x,y,z> and warpx.m_current_centering_no<x,y,z>
+        # Initialize warpx.field_centering_no<x,y,z> and warpx.current_centering_no<x,y,z>
         # if set by the user in the input (need to access grid info from solver attribute)
         # warpx.field_centering_no<x,y,z>
         if self.field_centering_order is not None:
