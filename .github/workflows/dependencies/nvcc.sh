f60a2a485236baa9f45f0364b8803499ef1763ab
Edoardo Zoni <ezoni@lbl.gov>
2025-03-04 10:32:50 -0800

    NVCC 12.6

--- .github/workflows/dependencies/nvcc.sh
+++ .github/workflows/dependencies/nvcc.sh
@@ -29,32 +29,34 @@ sudo apt-get install -y \
 # ccache
 $(dirname "$0")/ccache.sh
 
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb
-sudo dpkg -i cuda-keyring_1.0-1_all.deb
+# parse version number from command line argument
+VERSION_DOTTED=${1-12.6} && VERSION_DASHED=$(sed 's/\./-/' <<< ${VERSION_DOTTED})
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
 
 sudo apt-get update
 sudo apt-get install -y          \
-    cuda-command-line-tools-11-3 \
-    cuda-compiler-11-3           \
-    cuda-cupti-dev-11-3          \
-    cuda-minimal-build-11-3      \
-    cuda-nvml-dev-11-3           \
-    cuda-nvtx-11-3               \
-    libcufft-dev-11-3            \
-    libcurand-dev-11-3           \
-    libcusparse-dev-11-3
-sudo ln -s cuda-11.3 /usr/local/cuda
+    cuda-command-line-tools-${VERSION_DASHED} \
+    cuda-compiler-${VERSION_DASHED}           \
+    cuda-cupti-dev-${VERSION_DASHED}          \
+    cuda-minimal-build-${VERSION_DASHED}      \
+    cuda-nvml-dev-${VERSION_DASHED}           \
+    cuda-nvtx-${VERSION_DASHED}               \
+    libcufft-dev-${VERSION_DASHED}            \
+    libcurand-dev-${VERSION_DASHED}           \
+    libcusparse-dev-${VERSION_DASHED}
+sudo ln -s cuda-${VERSION_DOTTED} /usr/local/cuda
 
 # if we run out of temporary storage in CI:
-#du -sh /usr/local/cuda-11.3
+#du -sh /usr/local/cuda-${VERSION_DOTTED}
 #echo "+++ REDUCING CUDA Toolkit install size +++"
-#sudo rm -rf /usr/local/cuda-11.3/targets/x86_64-linux/lib/libcu{fft,pti,rand}_static.a
-#sudo rm -rf /usr/local/cuda-11.3/targets/x86_64-linux/lib/libnvperf_host_static.a
-#du -sh /usr/local/cuda-11.3/
+#sudo rm -rf /usr/local/cuda-${VERSION_DOTTED}/targets/x86_64-linux/lib/libcu{fft,pti,rand}_static.a
+#sudo rm -rf /usr/local/cuda-${VERSION_DOTTED}/targets/x86_64-linux/lib/libnvperf_host_static.a
+#du -sh /usr/local/cuda-${VERSION_DOTTED}/
 #df -h
 
 # cmake-easyinstall
-#
 sudo curl -L -o /usr/local/bin/cmake-easyinstall https://raw.githubusercontent.com/ax3l/cmake-easyinstall/main/cmake-easyinstall
 sudo chmod a+x /usr/local/bin/cmake-easyinstall
 export CEI_SUDO="sudo"
