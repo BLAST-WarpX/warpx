9070c0de577de31446e96104c13343ad6142c44f
Edoardo Zoni <ezoni@lbl.gov>
2025-03-04 14:40:45 -0800

    Use 25.03 tag for AMReX update

--- cmake/dependencies/AMReX.cmake
+++ cmake/dependencies/AMReX.cmake
@@ -294,7 +294,7 @@ set(WarpX_amrex_src ""
 set(WarpX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(WarpX_amrex_internal)")
-set(WarpX_amrex_branch "247628b112a5e0162e5626ed6de81f28687f31ea"
+set(WarpX_amrex_branch "25.03"
     CACHE STRING
     "Repository branch for WarpX_amrex_repo if(WarpX_amrex_internal)")
 
