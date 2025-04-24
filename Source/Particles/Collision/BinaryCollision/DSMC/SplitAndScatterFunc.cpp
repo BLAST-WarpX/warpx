39fd192e1c73958c10feadc9d19ecc5ddcf58a76
Remi Lehe <remi.lehe@normalesup.org>
2025-04-24 11:22:45 -0700

    Adjust number of products

--- Source/Particles/Collision/BinaryCollision/DSMC/SplitAndScatterFunc.cpp
+++ Source/Particles/Collision/BinaryCollision/DSMC/SplitAndScatterFunc.cpp
@@ -25,6 +25,8 @@ SplitAndScatterFunc::SplitAndScatterFunc (const std::string& collision_name,
 
         const bool ionization_flag = (!product_species.empty());
 
+        amrex::Print() << "ionization_flag = " << ionization_flag << std::endl;
+
         // if ionization is one of the processes, check if one of the colliding
         // species is also used as a product species
         if (ionization_flag) {
@@ -37,23 +39,25 @@ SplitAndScatterFunc::SplitAndScatterFunc (const std::string& collision_name,
             pp_collision_name.query("ionization_target_species", target_species);
             // find the index of the non-target species (the one that could
             // also be used as a product species)
-            int non_target_idx = 0;
-            if (colliding_species[0] == target_species) {
-                non_target_idx = 1;
-            }
+            AMREX_ALWAYS_ASSERT_WITH_MESSAGE( colliding_species[1]==target_species, "The target species is not the second colliding species.");
+            amrex::Print() << "colliding_species[0] = " << colliding_species[0] << std::endl;
+            amrex::Print() << "colliding_species[1] = " << colliding_species[1] << std::endl;
+            amrex::Print() << "target_species = " << target_species << std::endl;
 
             // check if the non-target species is in ``product_species``
-            auto it = std::find(product_species.begin(), product_species.end(), colliding_species[non_target_idx]);
+            auto it = std::find(product_species.begin(), product_species.end(), target_species);
 
             if (it != product_species.end()) {
+                amrex::Print() << "Target species is in product_species" << std::endl;
                 m_num_product_species = 3;
-                m_num_products_host.push_back(2); // the non-target species
-                m_num_products_host.push_back(1); // the target species
+                m_num_products_host.push_back(2); // the non-target species: one particle for the product ; one particle for the fact that the incident particle loses energy
+                m_num_products_host.push_back(0); // the target species
                 m_num_products_host.push_back(1); // corresponds to whichever ionization product species1 is not (ion or electron)
             } else {
+                amrex::Print() << "Target species is not in product_species" << std::endl;
                 m_num_product_species = 4;
                 m_num_products_host.push_back(1); // the non-target species
-                m_num_products_host.push_back(1); // the target species
+                m_num_products_host.push_back(0); // the target species
                 m_num_products_host.push_back(1); // first product species
                 m_num_products_host.push_back(1); // second product species
             }
