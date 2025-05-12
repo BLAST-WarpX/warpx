5162ec9e6e7a2dedd9992d927cc512d8fa2a930c
Remi Lehe <remi.lehe@normalesup.org>
2025-05-12 15:56:47 -0700

    Fix typo

--- Source/FieldSolver/FiniteDifferenceSolver/ComputeCurlA.cpp
+++ Source/FieldSolver/FiniteDifferenceSolver/ComputeCurlA.cpp
@@ -279,6 +279,7 @@ void FiniteDifferenceSolver::ComputeCurlASpherical (
 
                 Real const r = rmin + (i + 0.5_rt)*dr; // r on a cell-centered grid (Bt is cell-centered in r)
                 Bt(i, j, 0, 0) =  -T_Algo::UpwardDrr_over_r(Ap, r, dr, coefs_r, n_coefs_r, i, j, 0, 0);
+            },
 
             // Bp calculation
             [=] AMREX_GPU_DEVICE (int i, int j, int /*k*/){
