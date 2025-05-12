ca3f503d23d7fcd142a5267e151a414d6666c440
David Grote <dpgrote@lbl.gov>
2025-05-12 14:30:08 -0700

    Clean up Examples/Tests/langmuir/analysis_r1d.py
    Co-authored-by: Edoardo Zoni <59625522+EZoni@users.noreply.github.com>

--- Examples/Tests/langmuir/analysis_r1d.py
+++ Examples/Tests/langmuir/analysis_r1d.py
@@ -26,7 +26,6 @@ yt.funcs.mylog.setLevel(50)
 import numpy as np
 from scipy.constants import c, e, epsilon_0, m_e
 
-sys.path.insert(1, "../../../../warpx/Regression/Checksum/")
 
 # this will be the name of the plot file
 fn = sys.argv[1]
