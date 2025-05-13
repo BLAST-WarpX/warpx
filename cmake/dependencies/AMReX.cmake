7b9bd671a06e08428164d05648b4bbbd68c395e5
Edoardo Zoni <ezoni@lbl.gov>
2025-05-13 15:08:53 -0700

    Fetch AMReX bug fix

--- cmake/dependencies/AMReX.cmake
+++ cmake/dependencies/AMReX.cmake
@@ -294,7 +294,7 @@ set(WarpX_amrex_src ""
 set(WarpX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(WarpX_amrex_internal)")
-set(WarpX_amrex_branch "8976d06bbb13aae73208009aff1ced43c42a7d27"
+set(WarpX_amrex_branch "7911bc643bffed673c92a89db33303855ece05ea"
     CACHE STRING
     "Repository branch for WarpX_amrex_repo if(WarpX_amrex_internal)")
 
