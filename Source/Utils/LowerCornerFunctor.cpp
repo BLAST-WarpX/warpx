a82b0361f394d141b0b8aa4059fcc7427f83bad6
Luca Fedeli <luca.fedeli@cea.fr>
2025-05-19 13:33:45 +0200

    add using namespace amrex

--- Source/Utils/LowerCornerFunctor.cpp
+++ Source/Utils/LowerCornerFunctor.cpp
@@ -4,30 +4,32 @@
 
 #include <limits>
 
+using namespace amrex;
+
 LowerCornerFunctor::LowerCornerFunctor(
-    const amrex::Vector<amrex::Real> cur_time,
-    const amrex::Real time_of_last_gal_shift,
-    const amrex::Vector<amrex::Real> v_galilean,
-    const amrex::Vector<amrex::Geometry> geom):
+    const Vector<Real> cur_time,
+    const Real time_of_last_gal_shift,
+    const Vector<Real> v_galilean,
+    const Vector<Geometry> geom):
     m_cur_time{cur_time},
     m_time_of_last_gal_shift{time_of_last_gal_shift},
     m_v_galilean{v_galilean},
     m_geom{geom}
 {}
 
-amrex::XDim3
+XDim3
 LowerCornerFunctor::operator()(
-    const amrex::Box& bx, const int lev, const amrex::Real time_shift_delta) const
+    const Box& bx, const int lev, const Real time_shift_delta) const
 {
 
     const auto gm_lev = m_geom[lev];
-    const auto grid_box = amrex::RealBox{bx, gm_lev.CellSize(), gm_lev.ProbLo()};
-    const amrex::Real* grid_min = grid_box.lo();
+    const auto grid_box = RealBox{bx, gm_lev.CellSize(), gm_lev.ProbLo()};
+    const Real* grid_min = grid_box.lo();
 
-    const amrex::Real cur_time = m_cur_time[lev];
-    const amrex::Real time_shift =
+    const Real cur_time = m_cur_time[lev];
+    const Real time_shift =
         (cur_time + time_shift_delta - m_time_of_last_gal_shift);
-    amrex::Array<amrex::Real,3> galilean_shift = { m_v_galilean[0]*time_shift,
+    Array<Real,3> galilean_shift = { m_v_galilean[0]*time_shift,
                                                    m_v_galilean[1]*time_shift,
                                                    m_v_galilean[2]*time_shift};
 #if defined(WARPX_DIM_3D)
@@ -37,7 +39,7 @@ LowerCornerFunctor::operator()(
     return { grid_min[0] + galilean_shift[0], std::numeric_limits<Real>::lowest(), grid_min[1] + galilean_shift[2] };
 
 #elif defined(WARPX_DIM_RCYLINDER) || defined(WARPX_DIM_RSPHERE)
-    amrex::ignore_unused(galilean_shift);
+    ignore_unused(galilean_shift);
     return { grid_min[0], std::numeric_limits<Real>::lowest(), std::numeric_limits<Real>::lowest() };
 
 #elif defined(WARPX_DIM_1D_Z)
