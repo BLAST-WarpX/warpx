ce5ba276cef480564bc409dfec84698e63c9b860
Justin Angus <angus1@llnl.gov>
2025-05-23 17:31:29 -0700

    adding CI test for 2D thermal plasma with 4 PMC boundaries.

---
+++ Examples/Tests/implicit/analysis_implicit_2d.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+# Copyright 2024 Justin Angus
+#
+#
+# This file is part of WarpX.
+#
+# License: BSD-3-Clause-LBNL
+#
+# This is a script that analyses the simulation results from the script `inputs_vandb_2d`.
+# This simulates a 2D periodic plasma using the implicit solver
+# with the Villasenor deposition using shape factor 2.
+import sys
+
+import numpy as np
+import yt
+from scipy.constants import e, epsilon_0
+ep0_warpx = 8.8541878188e-12
+
+field_energy = np.loadtxt("diags/reducedfiles/field_energy.txt", skiprows=1)
+particle_energy = np.loadtxt("diags/reducedfiles/particle_energy.txt", skiprows=1)
+
+total_energy = field_energy[:, 2] + particle_energy[:, 2]
+
+delta_E = (total_energy - total_energy[0]) / total_energy[0]
+max_delta_E = np.abs(delta_E).max()
+
+# This case should have near machine precision conservation of charge and energy
+tolerance_rel_energy = 2.0e-14
+tolerance_rel_charge = 2.0e-14
+
+print(f"max change in energy: {max_delta_E}")
+print(f"tolerance: {tolerance_rel_energy}")
+
+assert max_delta_E < tolerance_rel_energy
+
+# check for machine precision conservation of charge density
+
+pltdir = sys.argv[1]
+ds = yt.load(pltdir)
+data = ds.covering_grid(
+    level=0, left_edge=ds.domain_left_edge, dims=ds.domain_dimensions
+)
+
+divE = data["boxlib", "divE"].value
+rho = data["boxlib", "rho"].value
+num = data["boxlib", "num_electrons"].value
+dLx = ds.domain_right_edge[0] - ds.domain_left_edge[0]
+dLz = ds.domain_right_edge[1] - ds.domain_left_edge[1]
+dV = dLx.value*dLz.value
+ne0 = num.sum()/dV
+
+# compute local error in Gauss's law
+drho = (rho - ep0_warpx * divE) / e / ne0
+
+# compute RMS on in error on the grid
+nX = drho.shape[0]
+nZ = drho.shape[1]
+drho2_avg = (drho**2).sum() / (nX * nZ)
+drho_rms = np.sqrt(drho2_avg)
+
+print(f"rms error in charge conservation: {drho_rms}")
+print(f"tolerance: {tolerance_rel_charge}")
+
+assert drho_rms < tolerance_rel_charge
