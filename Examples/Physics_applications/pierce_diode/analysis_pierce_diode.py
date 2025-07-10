9b60c46d37683d666c9de0f22082991283f81737
Olga Shapoval <oshapoval@lbl.gov>
2025-07-09 17:34:56 -0700

    Added check on phi

--- Examples/Physics_applications/pierce_diode/analysis_pierce_diode.py
+++ Examples/Physics_applications/pierce_diode/analysis_pierce_diode.py
@@ -28,19 +28,50 @@ J_CL = (
     * abs(extractor_voltage) ** (3 / 2)
     / d_plate**2
 )
-jz_t_last, meta = ts.get_field("j", "z", plot=False, iteration=ts.iterations[-1])
+
+it = ts.iterations[-1]
 time_cur = ts.current_t
-plt.figure()
-plt.plot(meta.z, jz_t_last)
-plt.axhline(y=J_CL, color="black", linestyle="-", label=r"$y = J_{CL \ limit}$")
-plt.xlabel("z, mm")
-plt.ylabel(
-    r"$J_z, A/m^2$ at " + "t = " + str(np.round(time_cur / 1e-6, 5)) + r" $ \mu s$"
+phi, meta = ts.get_field("phi", iteration=it, plot=False)
+phi_analyt = extractor_voltage * (meta.z / d_plate) ** (4 / 3)
+ez, _ = ts.get_field("E", "z", iteration=ts.iterations[-1], plot=False)
+rho, _ = ts.get_field("rho", iteration=it, plot=False)
+jz, _ = ts.get_field("j", "z", iteration=it, plot=False)
+z, uz = ts.get_particle(["z", "uz"], iteration=it)
+
+color = "orange"
+title = r"$\Gamma_{ions}=38.79 \approx \Gamma_{CL}$"
+
+fig, axs = plt.subplots(2, 2, figsize=(10, 8))
+fig.suptitle("time = " + str(np.round(time_cur / 1e-6, 5)) + r" $ \mu s$")
+axs[0, 0].scatter(z, uz, color=color, label=title, s=0.2)
+axs[0, 0].set_title(r"$u_z$")
+axs[0, 0].set_xlabel("z, mm")
+
+axs[0, 1].plot(meta.z, ez, color=color, label=title)
+axs[0, 1].set_title(r"$E_z$")
+axs[0, 1].set_xlabel("z, mm")
+axs[0, 1].legend()
+
+axs[1, 0].plot(meta.z, jz, color=color)
+axs[1, 0].set_title(r"$J_z$")
+axs[1, 0].axhline(
+    y=J_CL, color="black", linestyle="-", label=r"$y = J_{CL}$ (Child-Langmuir limit)"
 )
-plt.legend()
-plt.ylim([30, 40])
-plt.savefig("pierce_diode_1d_analysis.png")
-tolerance = 0.20  # corresponds to 20 %
-assert np.all(np.abs(jz_t_last - J_CL) / J_CL <= tolerance), (
-    "Child–Langmuir limit is violated!"
+axs[1, 0].set_xlabel("z, mm")
+axs[1, 0].legend(loc="upper left")
+
+axs[1, 1].plot(meta.z, phi, color=color)  # , label='WarpX')
+axs[1, 1].set_title(r"$phi$")
+axs[1, 1].set_xlabel("z, mm")
+axs[1, 1].plot(meta.z, phi_analyt, label="theory", ls=":", color="black")
+axs[1, 1].legend()
+plt.tight_layout()
+plt.show()
+
+rel_error_phi = np.abs(phi[1:] - phi_analyt[1:]) / np.abs(phi_analyt[1:])
+rel_error_jz = np.abs(jz - J_CL) / J_CL
+tolerance = 0.2
+
+assert np.all(rel_error_jz < tolerance) and np.all(rel_error_phi < tolerance), (
+    "Child–Langmuir limit is violated! "
 )
