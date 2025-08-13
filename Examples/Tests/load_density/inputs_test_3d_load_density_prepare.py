7d19094bf127ba4342b04bf2b8690174898ca348
Remi Lehe <remi.lehe@normalesup.org>
2025-08-13 14:20:10 -0700

    Cleaner script

--- Examples/Tests/load_density/inputs_test_3d_load_density_prepare.py
+++ Examples/Tests/load_density/inputs_test_3d_load_density_prepare.py
@@ -7,23 +7,38 @@ the WarpX particles should be initialized (on a 3D Cartesian grid)
 import numpy as np
 import openpmd_api as io
 
-density_data = np.zeros((41, 41, 41))
-density_data[:, :, :] = np.arange(41)[np.newaxis, np.newaxis, :]
+# Define density as a function of x, y, z, using numpy syntax
+# parabolic channel in x, y, with a ramp and plateau in z
+on_axis_density = 1e24  # m^-3
+channel_radius = 10e-6  # m
+ramp_length = 1e-3  # m
+# - Define the grid
+x_1d = np.linspace(-20e-6, 20e-6, 200)
+y_1d = np.linspace(-20e-6, 20e-6, 200)
+z_1d = np.linspace(0, 1e-2, 200)
+x, y, z = np.meshgrid(x_1d, y_1d, z_1d, indexing="ij")
+# - Define density as a function of x, y, z
+density_data = (
+    on_axis_density
+    * (1 + (x**2 + y**2) / channel_radius**2)
+    * np.where(z < ramp_length, z / ramp_length, 1)
+)
+density_data[z < 0] = 0
 
 # create openpmd file
 series = io.Series("example-density.h5", io.Access.create)
-# only 1 iteratiion needed
+# only 1 iteration needed
 it = series.iterations[1]
 # set meta information
 density = it.meshes["density"]
-density.grid_spacing = np.array([1e-6, 1e-6, 1e-6])
-density.grid_global_offset = [-20e-6, -20e-6, -20e-6]
+density.grid_spacing = np.array(
+    [x_1d[1] - x_1d[0], y_1d[1] - y_1d[0], z_1d[1] - z_1d[0]]
+)
+density.grid_global_offset = [x_1d.min(), y_1d.min(), z_1d.min()]
 density.axis_labels = ["x", "y", "z"]
 density.geometry = io.Geometry.cartesian
 density.unit_dimension = {
     io.Unit_Dimension.L: -3,
-    io.Unit_Dimension.I: 1,
-    io.Unit_Dimension.T: 1,
 }
 
 # label
