1fdd109855f9d8d406f341108861a6e7be712913
David Grote <grote1@llnl.gov>
2025-08-22 16:20:32 -0700

    Reworked the memory handling, making explicit the copy of the data to the device

--- Source/Particles/Collision/BinaryCollision/Bremsstrahlung/BremsstrahlungFunc.cpp
+++ Source/Particles/Collision/BinaryCollision/Bremsstrahlung/BremsstrahlungFunc.cpp
@@ -55,33 +55,42 @@ BremsstrahlungFunc::UploadCrossSection (int Z)
 
     constexpr auto m_e_eV = PhysConst::m_e*PhysConst::c*PhysConst::c/PhysConst::q_e; // 0.511e6
 
+    nkoT1 = static_cast<int>(m_koT1_grid_h.size());
+    nKE = static_cast<int>(m_KEgrid_eV_h.size());
+
+    m_exe.nkoT1 = nkoT1;
+    m_exe.nKE = nKE;
+
+    m_sigma_total_h.resize(nKE);
+    m_kdsigdk_h.resize(nKE*nkoT1);
+
     std::vector<std::vector<amrex::ParticleReal>> & kdsigdk = m_kdsigdk_map.at(Z);
 
     // kdsigdk at the default cutoff
     int const i0_cut = 0;
     amrex::ParticleReal const koT1_cut = m_exe.m_koT1_cut_default;
-    amrex::ParticleReal const w00 = (m_exe.m_koT1_grid[i0_cut+1] - koT1_cut)/(m_exe.m_koT1_grid[i0_cut+1] - m_exe.m_koT1_grid[i0_cut]);
+    amrex::ParticleReal const w00 = (m_koT1_grid_h[i0_cut+1] - koT1_cut)/(m_koT1_grid_h[i0_cut+1] - m_koT1_grid_h[i0_cut]);
 
     // Convert Seltzer and Berger energy-weighted differential cross section to units of [m^2]
-    for (int iee=0; iee < Executor::nKE; iee++) {
-        amrex::ParticleReal const E = m_exe.m_KEgrid_eV[iee]/m_e_eV;
+    for (int iee=0; iee < nKE; iee++) {
+        amrex::ParticleReal const E = m_KEgrid_eV_h[iee]/m_e_eV;
         amrex::ParticleReal const gamma = 1.0_prt + E;
         /* betaSq = 1.0 - 1.0/gamma/gamma */
         amrex::ParticleReal const betaSq = (E*E + 2._prt*E)/gamma/gamma;
         // 1.0e-31 converts mBarn to m**2
         amrex::ParticleReal const scale_factor = 1.0e-31_prt*Z*Z/betaSq;
-        for (int i=0; i < Executor::nkoT1; i++) {
-            m_exe.m_kdsigdk[iee][i] = kdsigdk[iee][i]*scale_factor;
+        for (int i=0; i < nkoT1; i++) {
+            m_kdsigdk_h[iee*nkoT1 + i] = kdsigdk[iee][i]*scale_factor;
         }
 
         // Calculate the total cross section using the default k-cutoff
-        amrex::ParticleReal const kdsigdk_cut = w00*m_exe.m_kdsigdk[iee][i0_cut] + (1.0_prt - w00)*m_exe.m_kdsigdk[iee][i0_cut+1];
+        amrex::ParticleReal const kdsigdk_cut = w00*m_kdsigdk_h[iee*nkoT1 + i0_cut] + (1.0_prt - w00)*m_kdsigdk_h[iee*nkoT1 + i0_cut+1];
         amrex::ParticleReal kdsigdk_im1 = kdsigdk_cut;
         amrex::ParticleReal koT1_im1 = koT1_cut;
         amrex::ParticleReal sigma = 0.0_prt;
-        for (int i=i0_cut+1; i < Executor::nkoT1; i++) {
-            amrex::ParticleReal const koT1_i = m_exe.m_koT1_grid[i];
-            amrex::ParticleReal const kdsigdk_i = m_exe.m_kdsigdk[iee][i];
+        for (int i=i0_cut+1; i < nkoT1; i++) {
+            amrex::ParticleReal const koT1_i = m_koT1_grid_h[i];
+            amrex::ParticleReal const kdsigdk_i = m_kdsigdk_h[iee*nkoT1 + i];
             amrex::ParticleReal const dk = (koT1_i - koT1_im1);
             amrex::ParticleReal const k_ave = (koT1_i + koT1_im1)*0.5_prt;
             amrex::ParticleReal const dsigdk = (kdsigdk_i + kdsigdk_im1)*0.5_prt/k_ave;
@@ -89,7 +98,24 @@ BremsstrahlungFunc::UploadCrossSection (int Z)
             koT1_im1 = koT1_i;
             kdsigdk_im1 = kdsigdk_i;
         }
-        m_exe.m_sigma_total[iee] = sigma;
+        m_sigma_total_h[iee] = sigma;
     }
+
+    // Setup and transfer the data to the GPU device
+    m_koT1_grid_d.resize(m_koT1_grid_h.size());
+    m_KEgrid_eV_d.resize(m_KEgrid_eV_h.size());
+    m_kdsigdk_d.resize(m_kdsigdk_h.size());
+    m_sigma_total_d.resize(m_sigma_total_h.size());
+
+    amrex::Gpu::copyAsync(amrex::Gpu::hostToDevice, m_koT1_grid_h.begin(), m_koT1_grid_h.end(), m_koT1_grid_d.begin());
+    amrex::Gpu::copyAsync(amrex::Gpu::hostToDevice, m_KEgrid_eV_h.begin(), m_KEgrid_eV_h.end(), m_KEgrid_eV_d.begin());
+    amrex::Gpu::copyAsync(amrex::Gpu::hostToDevice, m_kdsigdk_h.begin(), m_kdsigdk_h.end(), m_kdsigdk_d.begin());
+    amrex::Gpu::copyAsync(amrex::Gpu::hostToDevice, m_sigma_total_h.begin(), m_sigma_total_h.end(), m_sigma_total_d.begin());
+
+    // The kernels access the data via these pointers
+    m_exe.m_koT1_grid = m_koT1_grid_d.dataPtr();
+    m_exe.m_KEgrid_eV = m_KEgrid_eV_d.dataPtr();
+    m_exe.m_kdsigdk = m_kdsigdk_d.dataPtr();
+    m_exe.m_sigma_total = m_sigma_total_d.dataPtr();
 
 }
