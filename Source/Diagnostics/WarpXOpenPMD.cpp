fb2bd4803d6e7adb28cb87feb96193518bbd1a3d
Junmin Gu <junmin@login10.frontier.olcf.ornl.gov>
2025-08-27 20:13:24 -0400

    removed unused var

--- Source/Diagnostics/WarpXOpenPMD.cpp
+++ Source/Diagnostics/WarpXOpenPMD.cpp
@@ -1506,7 +1506,6 @@ WarpXOpenPMDPlot::WriteOpenPMDFieldsAll ( //const std::string& filename,
             }
         } // icomp setup loop
 
-        bool spanWorks = true;
         for ( int icomp=0; icomp<ncomp; icomp++ ) {
             std::string const & varname = varnames[icomp];
 
@@ -1547,9 +1546,9 @@ WarpXOpenPMDPlot::WriteOpenPMDFieldsAll ( //const std::string& filename,
                         WARPX_PROFILE("WarpXOpenPMDPlot::WriteOpenPMDFields::D2H_Span()");
                         auto dynamicMemoryView = mesh_comp.storeChunk<amrex::Real>(
                                                                                    chunk_offset, chunk_size,
-                                                                                   [&local_box, &spanWorks](size_t size) {
+                                                                                   [&local_box](size_t size) {
                                                                                       (void) size;
-                                                                                      spanWorks = false;
+                                                                                      amrex::Print()<<" span failed \n";
                                                                                       amrex::BaseFab<amrex::Real> foo(local_box, 1, amrex::The_Pinned_Arena());
                                                                                       std::shared_ptr<amrex::Real> data_pinned(foo.release());
                                                                                       return data_pinned;
@@ -1557,8 +1556,6 @@ WarpXOpenPMDPlot::WriteOpenPMDFieldsAll ( //const std::string& filename,
 
                         auto span = dynamicMemoryView.currentBuffer();
                         amrex::Gpu::dtoh_memcpy_async(span.data(), fab.dataPtr(icomp), local_box.numPts()*sizeof(amrex::Real));
-                        if (!spanWorks)
-                            amrex::Print()<<" span failed \n";
                     }
                 }  else
 #endif
