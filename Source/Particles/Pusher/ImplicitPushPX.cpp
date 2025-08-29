2e1037f4f999d3ad6aad6fde4dadc2fbc6b446f3
David Grote <grote1@llnl.gov>
2025-08-29 14:34:16 -0700

    Fix rhop to approximately include the coupling between substeps

--- Source/Particles/Pusher/ImplicitPushPX.cpp
+++ Source/Particles/Pusher/ImplicitPushPX.cpp
@@ -877,7 +877,7 @@ PhysicalParticleContainer::ImplicitPushXPSubOrbits (WarpXParIter& pti,
 
                 if (deposit_mass_matrices) {
                     const amrex::Real wq_invvol = wq*invvol/nsuborbits[ip];
-                    const amrex::Real rhop = wq*invvol*gaminv/nsuborbits[ip]; // use normal wq here
+                    const amrex::Real rhop = 2.0_rt*wq_invvol*gaminv; // use normal wq here
 
                     // Set the Mass Matrices kernels
                     amrex::ParticleReal fpxx, fpxy, fpxz;
