53f7835988a95e64e83b43c9ac897ad992b84055
Remi Lehe <remi.lehe@normalesup.org>
2025-09-22 15:55:31 -0700

    Update Source/Particles/MultiParticleContainer.cpp

--- Source/Particles/MultiParticleContainer.cpp
+++ Source/Particles/MultiParticleContainer.cpp
@@ -227,7 +227,7 @@ MultiParticleContainer::ReadParameters ()
         // can be provided in the input file. If not provided, it defaults to '1.0'
         if (m_E_ext_particle_s == "read_from_file") {
             // store the mathematical expression as string
-            std::string str_E_ext_time_function = 1.0;
+            std::string str_E_ext_time_function = "1.0";
             utils::parser::Query_parserString(
                 pp_particles, "read_fields_E_dependency(t)",
                 str_E_ext_time_function);
