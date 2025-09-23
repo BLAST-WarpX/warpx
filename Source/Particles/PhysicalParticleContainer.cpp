b9b777bc304cff4f218d9ff528869cbb730c1dde
Justin Angus <angus1@llnl.gov>
2025-09-23 13:45:05 -0700

    adding another comment.

--- Source/Particles/PhysicalParticleContainer.cpp
+++ Source/Particles/PhysicalParticleContainer.cpp
@@ -654,6 +654,7 @@ PhysicalParticleContainer::Evolve (ablastr::fields::MultiFabRegister& fields,
 
                     // Deposit inside domains
                     if (implicit_options && implicit_options->deposit_mass_matrices) {
+                        // Note that J for particles included in MM are deposited to current_fp_save
                         amrex::MultiFab * jx = fields.get(FieldType::current_fp_save, Direction{0}, lev);
                         amrex::MultiFab * jy = fields.get(FieldType::current_fp_save, Direction{1}, lev);
                         amrex::MultiFab * jz = fields.get(FieldType::current_fp_save, Direction{2}, lev);
