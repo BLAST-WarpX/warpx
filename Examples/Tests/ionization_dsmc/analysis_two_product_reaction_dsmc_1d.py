f8212a30e0f022febf3a0e1efb4eb04e0aee2e03
Olga Shapoval <oshapoval@lbl.gov>
2025-10-08 17:49:55 -0700

    Update analysis script

--- Examples/Tests/ionization_dsmc/analysis_two_product_reaction_dsmc_1d.py
+++ Examples/Tests/ionization_dsmc/analysis_two_product_reaction_dsmc_1d.py
@@ -1,27 +1,57 @@
 #!/usr/bin/env python3
 
 import numpy as np
-from scipy.constants import elementary_charge
+from openpmd_viewer import OpenPMDTimeSeries
+from scipy.constants import c as clight
 
-Q_eV = -1.66  # in [eV]
-Q_J = Q_eV * elementary_charge  # in [J]
+iteration = 100
 
-ekin_data = np.loadtxt("./diags/reducedfiles/EP.txt")
-ekin_total = ekin_data[:, 2]
-H_count = np.loadtxt("./diags/reducedfiles/PN.txt")[:, 3]
-total_energy = ekin_total + Q_J * H_count
+sim_uz = {"H": [], "H2plus": []}
+theory_uz = {"H": [], "H2plus": []}
 
-# Check energy and momentum conservation with for an absoluete tolerance (atol)
-assert np.all(np.isclose(total_energy, total_energy[1], rtol=0.0, atol=1e-3))
+# Read in the data
+ts = OpenPMDTimeSeries("./diags/diag")
 
+# Get H and H2+ species velocities from the simulation
+sim_uz["H"] = ts.get_particle(species="H", var_list=["uz"], iteration=iteration)[0]
+sim_uz["H2plus"] = ts.get_particle(
+    species="H2plus", var_list=["uz"], iteration=iteration
+)[0]
 
-def check_momentum_conservation():
-    total_momentum_x = np.loadtxt("./diags/reducedfiles/PP.txt")[:, 2]
-    total_momentum_y = np.loadtxt("./diags/reducedfiles/PP.txt")[:, 3]
-    total_momentum_z = np.loadtxt("./diags/reducedfiles/PP.txt")[:, 4]
-    assert np.allclose(total_momentum_x, total_momentum_x[0], rtol=0.0, atol=1e-7)
-    assert np.allclose(total_momentum_y, total_momentum_y[0], rtol=0.0, atol=1e-7)
-    assert np.allclose(total_momentum_z, total_momentum_z[0], rtol=0.0, atol=1e-7)
+# Calculate theoretical velocities for H and H2+ species for a given Q = -1.66 eV
+eV_to_J = 1.602e-19
+m_H = 1.6726e-27  # [kg]
+m_H2 = 2 * m_H  # [kg]
 
+E0 = 5.0  # [eV], initial kinetic energy of H+
+Q = -1.66  # [eV], reaction energy cost
 
-check_momentum_conservation()
+# Compute initial velocities
+v0 = np.sqrt(2 * E0 * eV_to_J / m_H)  # [m/s] initial H+ velocity
+v_H2 = 0.0  # [m/s] initial H2 velocity
+
+# Compute center-of-mass (COM) quantities
+v_com = (m_H * v0 + m_H2 * v_H2) / (m_H + m_H2)
+E_com_before = (
+    m_H2 / (m_H + m_H2)
+) * E0  # [eV] Total kinetic energy in the center-of-mass frame (before the reaction)
+# (e.i., E_com_before  = ( (m_H * m_H2 / (m_H + m_H2) ) * (v0-v_H2)**2 )/2)
+E_com_after = (
+    E_com_before + Q
+)  # [eV] Total kinetic energy in the center-of-mass frame (after the reaction)
+
+# Compute celocity of H in COM frame
+vH_com = np.sqrt(
+    4 / 3 * (E_com_after * eV_to_J) / m_H
+)  # [m/s] Note: in the COM frame, the total momentum is zero
+
+# Lab-frame velocities
+vH_lab = v_com + vH_com
+vH2_lab = v_com - vH_com / 2
+
+theory_uz["H"] = vH_lab / clight
+theory_uz["H2plus"] = vH2_lab / clight
+
+# Compare the fluxes
+assert np.allclose(sim_uz["H2plus"], theory_uz["H2plus"], atol=1e-8)
+assert np.allclose(sim_uz["H"], theory_uz["H"], atol=1e-8)
