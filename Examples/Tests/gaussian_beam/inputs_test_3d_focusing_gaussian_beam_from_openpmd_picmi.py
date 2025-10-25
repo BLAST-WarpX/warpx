562795335aa8e456c8d291d40e1c403c29193e01
Remi Lehe <remi.lehe@normalesup.org>
2025-10-25 16:40:32 -0700

    Add new test file

---
+++ Examples/Tests/gaussian_beam/inputs_test_3d_focusing_gaussian_beam_from_openpmd_picmi.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+# from warp import picmi
+import argparse
+
+from pywarpx import picmi
+
+constants = picmi.constants
+
+# Constants from the focusing beam example
+nano = 1.0e-9
+micro = 1.0e-6
+
+sigmax = 516.0 * nano
+sigmay = 7.7 * nano
+sigmaz = 300.0 * micro
+
+# Box dimensions
+Lx = 20 * sigmax
+Ly = 20 * sigmay
+Lz = 20 * sigmaz
+
+nx = 256
+ny = 256
+nz = 256
+
+xmin = -0.5 * Lx
+xmax = 0.5 * Lx
+ymin = -0.5 * Ly
+ymax = 0.5 * Ly
+zmin = -0.5 * Lz
+zmax = 0.5 * Lz
+
+em_order = 3
+
+grid = picmi.Cartesian3DGrid(
+    number_of_cells=[nx, ny, nz],
+    lower_bound=[xmin, ymin, zmin],
+    upper_bound=[xmax, ymax, zmax],
+    lower_boundary_conditions=["pec", "pec", "pec"],
+    upper_boundary_conditions=["pec", "pec", "pec"],
+    lower_boundary_conditions_particles=["absorbing", "absorbing", "absorbing"],
+    upper_boundary_conditions_particles=["absorbing", "absorbing", "absorbing"],
+    warpx_max_grid_size=256,
+)
+
+solver = picmi.ElectromagneticSolver(grid=grid)
+
+# Create species with external file injection
+
+beam1_distribution = picmi.FromFileDistribution(
+    file_path="../test_3d_focusing_gaussian_beam_from_openpmd_prepare/openpmd_generated_particles.h5",
+)
+
+beam1 = picmi.Species(
+    particle_type="electron",
+    name="beam1",
+    initial_distribution=beam1_distribution,
+)
+
+field_diag1 = picmi.FieldDiagnostic(
+    name="diag1",
+    grid=grid,
+    period=1,
+    data_list=args.fields_to_plot,
+    warpx_format=args.diagformat,
+    warpx_dump_last_timestep=1,
+)
+
+part_diag1 = picmi.ParticleDiagnostic(
+    name="openpmd",
+    period=1,
+    species=[beam1],
+    data_list=["weighting", "x", "y", "z"],
+    warpx_format="openpmd",
+    warpx_dump_last_timestep=1,
+)
+
+sim = picmi.Simulation(
+    solver=solver,
+    max_steps=0,
+    verbose=1,
+    warpx_particle_shape=3,
+)
+
+sim.add_species(beam1)
+
+sim.add_diagnostic(field_diag1)
+sim.add_diagnostic(part_diag1)
+
+# write_inputs will create an inputs file that can be used to run
+# with the compiled version.
+# sim.write_input_file(file_name = 'inputs_from_PICMI')
+
+# Alternatively, sim.step will run WarpX, controlling it from Python
+sim.step()
