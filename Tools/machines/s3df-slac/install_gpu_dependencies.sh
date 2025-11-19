c52028f178acb84f42aed735c0e88af9f108d4a5
Peter Kicsiny <pkicsiny@sdfiana008.sdf.slac.stanford.edu>
2025-11-19 15:30:21 -0800

    add gpu install scripts

--- Tools/machines/s3df-slac/install_gpu_dependencies.sh
+++ Tools/machines/s3df-slac/install_gpu_dependencies.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-# Copyright 2023 The WarpX Community
+# Copyright 2025 The WarpX Community
 #
 # This file is part of WarpX.
 #
-# Author: Axel Huebl
+# Author: Peter Kicsiny
 # License: BSD-3-Clause-LBNL
 
 # Exit on first error encountered #############################################
@@ -14,24 +14,25 @@ set -eu -o pipefail
 
 # Check: ######################################################################
 #
-#   Was perlmutter_gpu_warpx.profile sourced and configured correctly?
-if [ -z ${proj-} ]; then echo "WARNING: The 'proj' variable is not yet set in your perlmutter_gpu_warpx.profile file! Please edit its line 2 to continue!"; exit 1; fi
+#   Was s3df_gpu_warpx.profile sourced and configured correctly?
+if [ -z ${proj-} ]; then echo "WARNING: The 'proj' variable is not yet set in your s3df_gpu_warpx.profile file! Please edit its line 2 to continue!"; exit 1; fi
 
 
-# Check $proj variable is correct and has a corresponding CFS directory #######
+# Check $proj variable is correct and has a corresponding /sdf/group directory #######
 #
-if [ ! -d "${CFS}/${proj%_g}/" ]
+if [ ! -d "/sdf/group/${proj}/" ]
 then
-    echo "WARNING: The directory ${CFS}/${proj%_g}/ does not exist!"
+    echo "WARNING: The directory /sdf/group/${proj}/ does not exist!"
     echo "Is the \$proj environment variable of value \"$proj\" correctly set? "
-    echo "Please edit line 2 of your perlmutter_gpu_warpx.profile file to continue!"
+    echo "Please edit line 2 of your s3df_gpu_warpx.profile file to continue!"
     exit
 fi
 
 
 # Remove old dependencies #####################################################
 #
-SW_DIR="${PSCRATCH}/storage/sw/warpx/perlmutter/gpu"
+#SW_DIR="${PSCRATCH}/storage/sw/warpx/perlmutter/gpu"
+SW_DIR="/sdf/group/${proj}/${USER}/sw/warpx/s3df/gpu"
 rm -rf ${SW_DIR}
 mkdir -p ${SW_DIR}
 
@@ -50,13 +51,27 @@ PARALLEL=16
 # tmpfs build directory: avoids issues often seen with $HOME and is faster
 build_dir=$(mktemp -d)
 
+# cmake
+echo "installing cmake..."
+curl -Lo cmake.tar.gz https://github.com/Kitware/CMake/releases/download/v4.1.2/cmake-4.1.2.tar.gz
+tar -xzf cmake.tar.gz
+mv cmake-4.1.2 ${SW_DIR}/cmake-4.1.2
+rm -rf cmake.tar.gz
+cd ${SW_DIR}/cmake-4.1.2
+./bootstrap --prefix=${SW_DIR}/cmake-4.1.2
+make -j$(nproc)
+make install
+export PATH=${SW_DIR}/cmake-4.1.2/bin:$PATH
+
 # CCache
+echo "installing ccache..."
 curl -Lo ccache.tar.xz https://github.com/ccache/ccache/releases/download/v4.10.2/ccache-4.10.2-linux-x86_64.tar.xz
 tar -xf ccache.tar.xz
 mv ccache-4.10.2-linux-x86_64 ${SW_DIR}/ccache-4.10.2
 rm -rf ccache.tar.xz
 
 # Boost (QED tables)
+echo "installing boost..."
 rm -rf $HOME/src/boost-temp
 mkdir -p $HOME/src/boost-temp
 curl -Lo $HOME/src/boost-temp/boost.tar.gz https://archives.boost.io/release/1.82.0/source/boost_1_82_0.tar.gz
@@ -68,6 +83,7 @@ cd -
 rm -rf $HOME/src/boost-temp
 
 # c-blosc (I/O compression)
+echo "installing c-blosc"
 if [ -d $HOME/src/c-blosc ]
 then
   cd $HOME/src/c-blosc
@@ -78,11 +94,12 @@ else
   git clone -b v1.21.1 https://github.com/Blosc/c-blosc.git $HOME/src/c-blosc
 fi
 rm -rf $HOME/src/c-blosc-pm-gpu-build
-cmake -S $HOME/src/c-blosc -B ${build_dir}/c-blosc-pm-gpu-build -DBUILD_TESTS=OFF -DBUILD_BENCHMARKS=OFF -DDEACTIVATE_AVX2=OFF -DCMAKE_INSTALL_PREFIX=${SW_DIR}/c-blosc-1.21.1
+cmake -S $HOME/src/c-blosc -B ${build_dir}/c-blosc-pm-gpu-build -DBUILD_TESTS=OFF -DBUILD_BENCHMARKS=OFF -DDEACTIVATE_AVX2=OFF -DCMAKE_INSTALL_PREFIX=${SW_DIR}/c-blosc-1.21.1 -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 cmake --build ${build_dir}/c-blosc-pm-gpu-build --target install --parallel ${PARALLEL}
 rm -rf ${build_dir}/c-blosc-pm-gpu-build
 
 # ADIOS2
+echo "installing adios2..."
 if [ -d $HOME/src/adios2 ]
 then
   cd $HOME/src/adios2
@@ -97,7 +114,24 @@ cmake -S $HOME/src/adios2 -B ${build_dir}/adios2-pm-gpu-build -DADIOS2_USE_Blosc
 cmake --build ${build_dir}/adios2-pm-gpu-build --target install -j ${PARALLEL}
 rm -rf ${build_dir}/adios2-pm-gpu-build
 
+# OPENBLAS
+echo "installing openblas..."
+if [ -d $HOME/src/openblas ]
+then
+  cd $HOME/src/openblas
+  git fetch --prune
+  git checkout v0.3.30
+  cd -
+else
+  git clone -b v0.3.30 https://github.com/xianyi/OpenBLAS.git $HOME/src/openblas
+fi
+rm -rf $HOME/src/openblas-pm-gpu-build
+CXX=$(which CC) cmake -S $HOME/src/openblas -B ${build_dir}/openblas-pm-gpu-build -Duse_openmp=ON -Dgpu_backend=OFF -DCMAKE_CXX_STANDARD=17 -DCMAKE_INSTALL_PREFIX=${SW_DIR}/openblas-0.3.30 -DBUILD_SHARED_LIBS=ON
+cmake --build ${build_dir}/openblas-pm-gpu-build --target install --parallel ${PARALLEL}
+rm -rf ${build_dir}/openblas-pm-gpu-build
+
 # BLAS++ (for PSATD+RZ)
+echo "installing blas++..."
 if [ -d $HOME/src/blaspp ]
 then
   cd $HOME/src/blaspp
@@ -108,11 +142,12 @@ else
   git clone -b v2024.05.31 https://github.com/icl-utk-edu/blaspp.git $HOME/src/blaspp
 fi
 rm -rf $HOME/src/blaspp-pm-gpu-build
-CXX=$(which CC) cmake -S $HOME/src/blaspp -B ${build_dir}/blaspp-pm-gpu-build -Duse_openmp=OFF -Dgpu_backend=cuda -DCMAKE_CXX_STANDARD=17 -DCMAKE_INSTALL_PREFIX=${SW_DIR}/blaspp-2024.05.31
+CXX=$(which CC) cmake -S $HOME/src/blaspp -B ${build_dir}/blaspp-pm-gpu-build -Duse_openmp=OFF -Dgpu_backend=cuda -DCMAKE_CXX_STANDARD=17 -DCMAKE_INSTALL_PREFIX=${SW_DIR}/blaspp-2024.05.31 -DBLAS_LIBRARIES=${SW_DIR}/openblas-0.3.30/lib64/libopenblas.so -DBLAS_INCLUDE_DIR=${SW_DIR}/openblas-0.3.30/include
 cmake --build ${build_dir}/blaspp-pm-gpu-build --target install --parallel ${PARALLEL}
 rm -rf ${build_dir}/blaspp-pm-gpu-build
 
 # LAPACK++ (for PSATD+RZ)
+echo "installing lapack++..."
 if [ -d $HOME/src/lapackpp ]
 then
   cd $HOME/src/lapackpp
@@ -129,6 +164,7 @@ rm -rf ${build_dir}/lapackpp-pm-gpu-build
 
 # Python ######################################################################
 #
+echo "installing python packages..."
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade virtualenv
 python3 -m pip cache purge
