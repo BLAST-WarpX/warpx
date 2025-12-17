BLAST-WarpX/warpx

commit d87a0f5447492b857cf4be0333ecddb4c19b14b9

allow dt update based on particle CFL for theta implicit solver. (#6428)
This PR adds the ability to dynamically limit the time step based on the
particle CFL when using the theta implicit EM solver, which treats light
waves implicitly. This would be a one line PR if it wasn't for one
subtlety described below.

The call to `UpdateDtFromParticleSpeeds()` in the` WarpX::Evolve()`
routine occurs prior to the call to `OneStep()`. For many of the
time-advance methods, including the implicit ones, `OneStep()` first
performs collisions and then the PIC advance. However, to be meaningful,
the call to `UpdateDtFromParticleSpeeds()` should occur **just before**
the PIC advance. This PR swaps the order of calls inside `OneStep()` for
the implicit methods to do the PIC advance before collisions. This
ordering is actually more common in my experience, and I wonder if the
ordering should be swapped for other time-advance methods as well.
@roelof-groenewald @ax3l @RemiLehe

Source/Evolve/WarpXEvolve.cpp
@@ -190,10 +190,9 @@ WarpX::Evolve (int numsteps)
 
         CheckLoadBalance(step);
 
-        // Update timestep for electrostatic solver if a constant dt is not provided
-        // This first synchronizes the position and velocity before setting the new timestep
-        if (electromagnetic_solver_id == ElectromagneticSolverAlgo::None &&
-            !m_const_dt.has_value() && m_dt_update_interval.contains(step+1)) {
+        // Update the timestep for solvers that support adaptive timestepping
+        // (electrostatic and theta-implicit EM), provided const_dt is not specified.
+        if (m_dt_update_interval.contains(step+1)) {
             if (verbose_step) {
                 amrex::Print() << Utils::TextMsg::Info("updating timestep");
             }
@@ -384,13 +383,13 @@ void WarpX::OneStep (
 
     // implicit solver
     if (m_implicit_solver) {
+        // advance fields and particles by one time step
+        m_implicit_solver->OneStep(a_cur_time, a_dt, a_step);
+
         // perform particle collisions
         ExecutePythonCallback("beforecollisions");
         mypc->doCollisions(a_step, a_cur_time, a_dt);
         ExecutePythonCallback("aftercollisions");
-
-        // advance fields and particles by one time step
-        m_implicit_solver->OneStep(a_cur_time, a_dt, a_step);
     }
     // explicit solver
     else {

Source/WarpX.cpp
@@ -783,6 +783,17 @@ WarpX::ReadParameters ()
         std::vector<std::string> dt_interval_vec = {"-1"};
         pp_warpx.queryarr("dt_update_interval", dt_interval_vec);
         m_dt_update_interval = utils::parser::IntervalsParser(dt_interval_vec);
+        if (m_dt_update_interval.isActivated()) {
+            WARPX_ALWAYS_ASSERT_WITH_MESSAGE(
+                !m_const_dt.has_value(),
+                "warpx.const_dt and warpx.dt_update_interval cannot be defined simultaneously."
+            );
+            WARPX_ALWAYS_ASSERT_WITH_MESSAGE(
+                (electromagnetic_solver_id == ElectromagneticSolverAlgo::None ||
+                 evolve_scheme == EvolveScheme::ThetaImplicitEM),
+                "For electromagnetic solvers, warpx.dt_update_interval can only be used with algo.evolve_scheme = theta_implicit_em."
+            );
+        }
 
         // Filter defaults to true for the explicit scheme, and false for the implicit schemes
         if (evolve_scheme != EvolveScheme::Explicit) {

commit 09e1f8ea9d9eb242afcc12bcc8d54f3ff5aae9b2

collisions should be after call to HandleParticlesAtBoundaries() (#6458)
This PR moves the collisions for the implicit solver to the proper place
in the Evolve loop. This fixes a bug introduced in PR #6428, where the
collisions are moved from just before to just after the PIC advance.
(See https://github.com/BLAST-WarpX/warpx/issues/6449). However, with
this change I noticed odd behavior in simulation outputs at processor
boundaries late in time for highly collisional simulations. The fix is
that the collisions should be placed after the particle communication
and boundary handling is performed, which occurs later in the Evolve
loop.

Source/Evolve/WarpXEvolve.cpp
@@ -270,6 +270,12 @@ WarpX::Evolve (int numsteps)
 
         HandleParticlesAtBoundaries(step, cur_time, num_moved);
 
+        if (m_implicit_solver) {
+            ExecutePythonCallback("beforecollisions");
+            mypc->doCollisions(step, cur_time, dt[0]);
+            ExecutePythonCallback("aftercollisions");
+        }
+
         // Field solve step for electrostatic or hybrid-PIC solvers
         if( electrostatic_solver_id != ElectrostaticSolverAlgo::None ||
             electromagnetic_solver_id == ElectromagneticSolverAlgo::HybridPIC )
@@ -385,11 +391,6 @@ void WarpX::OneStep (
     if (m_implicit_solver) {
         // advance fields and particles by one time step
         m_implicit_solver->OneStep(a_cur_time, a_dt, a_step);
-
-        // perform particle collisions
-        ExecutePythonCallback("beforecollisions");
-        mypc->doCollisions(a_step, a_cur_time, a_dt);
-        ExecutePythonCallback("aftercollisions");
     }
     // explicit solver
     else {