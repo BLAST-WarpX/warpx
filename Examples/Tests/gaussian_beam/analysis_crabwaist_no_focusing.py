b8b05daff316633d6b286f7ec319c5860ced4b8a
Peter Kicsiny <pkicsiny@sdfiana008.sdf.slac.stanford.edu>
2026-01-13 13:31:46 -0800

    formatting

--- Examples/Tests/gaussian_beam/analysis_crabwaist_no_focusing.py
+++ Examples/Tests/gaussian_beam/analysis_crabwaist_no_focusing.py
@@ -14,11 +14,12 @@
 # In the test I apply the inverse rotation and CW on both bunches and
 # compare them cordinate by coordinate.
 
-import numpy as np
-import matplotlib.pyplot as plt
-from openpmd_viewer import OpenPMDTimeSeries
 import os
-from scipy.constants import alpha, c, eV, m_e, pi
+
+import matplotlib.pyplot as plt
+import numpy as np
+from openpmd_viewer import OpenPMDTimeSeries
+from scipy.constants import c, eV, m_e
 
 ###############
 # beam params #
@@ -28,25 +29,25 @@ bunchint = 1.55e11
 emit_x = 1e-6
 emit_y = 1e-6
 emit_z = 0
-sigma_x  = 1e-5 # [m]
-sigma_y  = 1e-5 # [m]
-sigma_z  = 1e-4 # [m]
-sigma_px = emit_x / sigma_x # [1]
-sigma_py = emit_y / sigma_y # [1]
-sigma_pz = emit_z / sigma_z # [1]
+sigma_x = 1e-5  # [m]
+sigma_y = 1e-5  # [m]
+sigma_z = 1e-4  # [m]
+sigma_px = emit_x / sigma_x  # [1]
+sigma_py = emit_y / sigma_y  # [1]
+sigma_pz = emit_z / sigma_z  # [1]
 
 GeV = 1e9 * eV
-energy = 182.5 * GeV # [J]
-m_e_ev = m_e * c**2/eV
-gamma = 182.5*1e9 / m_e_ev
+energy = 182.5 * GeV  # [J]
+m_e_ev = m_e * c**2 / eV
+gamma = 182.5 * 1e9 / m_e_ev
 
-phi = 1e-3 # [rad] half crossing angle
+phi = 1e-3  # [rad] half crossing angle
 
 n_macroparts = int(1e6)
 
 print(sigma_x, sigma_y, sigma_z, sigma_px, sigma_py, sigma_pz)
 
-crabwaist_strength = 0.4 # cw strength [1]
+crabwaist_strength = 0.4  # cw strength [1]
 
 ###################
 # load warpx data #
@@ -58,13 +59,53 @@ sim_folder = "."
 
 # sim 1: init a gaussian bunch and rotate by phi
 series = OpenPMDTimeSeries(os.path.join(sim_folder, "diags_cw_off", "diag1"))
-ux_ele_cw_0, uy_ele_cw_0, uz_ele_cw_0, w_ele_cw_0, x_ele_cw_0, y_ele_cw_0, z_ele_cw_0 = series.get_particle(["ux", "uy", "uz","w", "x", "y", "z"], species="beam1", iteration=0)
-ux_pos_cw_0, uy_pos_cw_0, uz_pos_cw_0, w_pos_cw_0, x_pos_cw_0, y_pos_cw_0, z_pos_cw_0 = series.get_particle(["ux", "uy", "uz","w", "x", "y", "z"], species="beam2", iteration=0)
+(
+    ux_ele_cw_0,
+    uy_ele_cw_0,
+    uz_ele_cw_0,
+    w_ele_cw_0,
+    x_ele_cw_0,
+    y_ele_cw_0,
+    z_ele_cw_0,
+) = series.get_particle(
+    ["ux", "uy", "uz", "w", "x", "y", "z"], species="beam1", iteration=0
+)
+(
+    ux_pos_cw_0,
+    uy_pos_cw_0,
+    uz_pos_cw_0,
+    w_pos_cw_0,
+    x_pos_cw_0,
+    y_pos_cw_0,
+    z_pos_cw_0,
+) = series.get_particle(
+    ["ux", "uy", "uz", "w", "x", "y", "z"], species="beam2", iteration=0
+)
 
 # sim 2: init gaussian bunch, apply cw, rotate by phi
 series = OpenPMDTimeSeries(os.path.join(sim_folder, "diags_cw_on", "diag1"))
-ux_ele_cw_1, uy_ele_cw_1, uz_ele_cw_1, w_ele_cw_1, x_ele_cw_1, y_ele_cw_1, z_ele_cw_1 = series.get_particle(["ux", "uy", "uz","w", "x", "y", "z"], species="beam1", iteration=0)
-ux_pos_cw_1, uy_pos_cw_1, uz_pos_cw_1, w_pos_cw_1, x_pos_cw_1, y_pos_cw_1, z_pos_cw_1 = series.get_particle(["ux", "uy", "uz","w", "x", "y", "z"], species="beam2", iteration=0)
+(
+    ux_ele_cw_1,
+    uy_ele_cw_1,
+    uz_ele_cw_1,
+    w_ele_cw_1,
+    x_ele_cw_1,
+    y_ele_cw_1,
+    z_ele_cw_1,
+) = series.get_particle(
+    ["ux", "uy", "uz", "w", "x", "y", "z"], species="beam1", iteration=0
+)
+(
+    ux_pos_cw_1,
+    uy_pos_cw_1,
+    uz_pos_cw_1,
+    w_pos_cw_1,
+    x_pos_cw_1,
+    y_pos_cw_1,
+    z_pos_cw_1,
+) = series.get_particle(
+    ["ux", "uy", "uz", "w", "x", "y", "z"], species="beam2", iteration=0
+)
 
 # sort all arrays according to this order
 idx_ele_cw_0 = np.argsort(x_ele_cw_0)
@@ -76,43 +117,43 @@ idx_pos_cw_1 = np.argsort(x_pos_cw_1)
 ux_ele_cw_0 = ux_ele_cw_0[idx_ele_cw_0]
 uy_ele_cw_0 = uy_ele_cw_0[idx_ele_cw_0]
 uz_ele_cw_0 = uz_ele_cw_0[idx_ele_cw_0]
-w_ele_cw_0  = w_ele_cw_0 [idx_ele_cw_0]
-x_ele_cw_0  = x_ele_cw_0 [idx_ele_cw_0]
-y_ele_cw_0  = y_ele_cw_0 [idx_ele_cw_0]
-z_ele_cw_0  = z_ele_cw_0 [idx_ele_cw_0]
+w_ele_cw_0 = w_ele_cw_0[idx_ele_cw_0]
+x_ele_cw_0 = x_ele_cw_0[idx_ele_cw_0]
+y_ele_cw_0 = y_ele_cw_0[idx_ele_cw_0]
+z_ele_cw_0 = z_ele_cw_0[idx_ele_cw_0]
 
 ux_pos_cw_0 = ux_pos_cw_0[idx_pos_cw_0]
 uy_pos_cw_0 = uy_pos_cw_0[idx_pos_cw_0]
 uz_pos_cw_0 = uz_pos_cw_0[idx_pos_cw_0]
-w_pos_cw_0  = w_pos_cw_0 [idx_pos_cw_0]
-x_pos_cw_0  = x_pos_cw_0 [idx_pos_cw_0]
-y_pos_cw_0  = y_pos_cw_0 [idx_pos_cw_0]
-z_pos_cw_0  = z_pos_cw_0 [idx_pos_cw_0]
+w_pos_cw_0 = w_pos_cw_0[idx_pos_cw_0]
+x_pos_cw_0 = x_pos_cw_0[idx_pos_cw_0]
+y_pos_cw_0 = y_pos_cw_0[idx_pos_cw_0]
+z_pos_cw_0 = z_pos_cw_0[idx_pos_cw_0]
 
 # second dataset, cw on
 ux_ele_cw_1 = ux_ele_cw_1[idx_ele_cw_1]
 uy_ele_cw_1 = uy_ele_cw_1[idx_ele_cw_1]
 uz_ele_cw_1 = uz_ele_cw_1[idx_ele_cw_1]
-w_ele_cw_1  = w_ele_cw_1 [idx_ele_cw_1]
-x_ele_cw_1  = x_ele_cw_1 [idx_ele_cw_1]
-y_ele_cw_1  = y_ele_cw_1 [idx_ele_cw_1]
-z_ele_cw_1  = z_ele_cw_1 [idx_ele_cw_1]
+w_ele_cw_1 = w_ele_cw_1[idx_ele_cw_1]
+x_ele_cw_1 = x_ele_cw_1[idx_ele_cw_1]
+y_ele_cw_1 = y_ele_cw_1[idx_ele_cw_1]
+z_ele_cw_1 = z_ele_cw_1[idx_ele_cw_1]
 
 ux_pos_cw_1 = ux_pos_cw_1[idx_pos_cw_1]
 uy_pos_cw_1 = uy_pos_cw_1[idx_pos_cw_1]
 uz_pos_cw_1 = uz_pos_cw_1[idx_pos_cw_1]
-w_pos_cw_1  = w_pos_cw_1 [idx_pos_cw_1]
-x_pos_cw_1  = x_pos_cw_1 [idx_pos_cw_1]
-y_pos_cw_1  = y_pos_cw_1 [idx_pos_cw_1]
-z_pos_cw_1  = z_pos_cw_1 [idx_pos_cw_1]
+w_pos_cw_1 = w_pos_cw_1[idx_pos_cw_1]
+x_pos_cw_1 = x_pos_cw_1[idx_pos_cw_1]
+y_pos_cw_1 = y_pos_cw_1[idx_pos_cw_1]
+z_pos_cw_1 = z_pos_cw_1[idx_pos_cw_1]
 
 # after sorting it should be the same for both datasets
-x_ele_pos_idx = x_ele_cw_0>0
-x_pos_pos_idx = x_pos_cw_0>0
+x_ele_pos_idx = x_ele_cw_0 > 0
+x_pos_pos_idx = x_pos_cw_0 > 0
 
 # sanity checks on sorting
-assert np.all(x_ele_pos_idx == (x_ele_cw_1>0))
-assert np.all(x_pos_pos_idx == (x_pos_cw_1>0))
+assert np.all(x_ele_pos_idx == (x_ele_cw_1 > 0))
+assert np.all(x_pos_pos_idx == (x_pos_cw_1 > 0))
 assert np.all(uy_ele_cw_0 == uy_ele_cw_1)
 assert np.all(uy_pos_cw_0 == uy_pos_cw_1)
 assert np.all(x_ele_cw_0 == x_ele_cw_1)
@@ -130,19 +171,63 @@ assert np.all(z_pos_cw_0 == z_pos_cw_1)
 # CW: crabwaist operation
 
 # sim 1
-fig, ax = plt.subplots(1,2, figsize=(10,3))
+fig, ax = plt.subplots(1, 2, figsize=(10, 3))
 
-ax[0].plot(x_ele_cw_0[ x_ele_pos_idx]/sigma_x, ux_ele_cw_0[ x_ele_pos_idx]/sigma_px, "o", c="darkred", label=r"e- ($Rx>0$)")
-ax[0].plot(x_ele_cw_0[~x_ele_pos_idx]/sigma_x, ux_ele_cw_0[~x_ele_pos_idx]/sigma_px, "o", c="darkblue", label=r"e- ($Rx<0$)")
+ax[0].plot(
+    x_ele_cw_0[x_ele_pos_idx] / sigma_x,
+    ux_ele_cw_0[x_ele_pos_idx] / sigma_px,
+    "o",
+    c="darkred",
+    label=r"e- ($Rx>0$)",
+)
+ax[0].plot(
+    x_ele_cw_0[~x_ele_pos_idx] / sigma_x,
+    ux_ele_cw_0[~x_ele_pos_idx] / sigma_px,
+    "o",
+    c="darkblue",
+    label=r"e- ($Rx<0$)",
+)
 
-ax[0].plot(x_pos_cw_0[ x_pos_pos_idx]/sigma_x, ux_pos_cw_0[ x_pos_pos_idx]/sigma_px, "rx", label=r"e+ ($Rx>0$)")
-ax[0].plot(x_pos_cw_0[~x_pos_pos_idx]/sigma_x, ux_pos_cw_0[~x_pos_pos_idx]/sigma_px, "bx", label=r"e+ ($Rx<0$)")
+ax[0].plot(
+    x_pos_cw_0[x_pos_pos_idx] / sigma_x,
+    ux_pos_cw_0[x_pos_pos_idx] / sigma_px,
+    "rx",
+    label=r"e+ ($Rx>0$)",
+)
+ax[0].plot(
+    x_pos_cw_0[~x_pos_pos_idx] / sigma_x,
+    ux_pos_cw_0[~x_pos_pos_idx] / sigma_px,
+    "bx",
+    label=r"e+ ($Rx<0$)",
+)
 
-ax[1].plot(y_ele_cw_0[ x_ele_pos_idx]/sigma_y, uy_ele_cw_0[ x_ele_pos_idx]/sigma_py, "o", c="darkred",  label=r"e- ($Rx>0$)")
-ax[1].plot(y_ele_cw_0[~x_ele_pos_idx]/sigma_y, uy_ele_cw_0[~x_ele_pos_idx]/sigma_py, "o", c="darkblue", label=r"e- ($Rx<0$)")
+ax[1].plot(
+    y_ele_cw_0[x_ele_pos_idx] / sigma_y,
+    uy_ele_cw_0[x_ele_pos_idx] / sigma_py,
+    "o",
+    c="darkred",
+    label=r"e- ($Rx>0$)",
+)
+ax[1].plot(
+    y_ele_cw_0[~x_ele_pos_idx] / sigma_y,
+    uy_ele_cw_0[~x_ele_pos_idx] / sigma_py,
+    "o",
+    c="darkblue",
+    label=r"e- ($Rx<0$)",
+)
 
-ax[1].plot(y_pos_cw_0[ x_pos_pos_idx]/sigma_y, uy_pos_cw_0[ x_pos_pos_idx]/sigma_py, "rx", label=r"e+ ($Rx>0$)")
-ax[1].plot(y_pos_cw_0[~x_pos_pos_idx]/sigma_y, uy_pos_cw_0[~x_pos_pos_idx]/sigma_py, "bx", label=r"e+ ($Rx<0$)")
+ax[1].plot(
+    y_pos_cw_0[x_pos_pos_idx] / sigma_y,
+    uy_pos_cw_0[x_pos_pos_idx] / sigma_py,
+    "rx",
+    label=r"e+ ($Rx>0$)",
+)
+ax[1].plot(
+    y_pos_cw_0[~x_pos_pos_idx] / sigma_y,
+    uy_pos_cw_0[~x_pos_pos_idx] / sigma_py,
+    "bx",
+    label=r"e+ ($Rx<0$)",
+)
 
 ax[0].set_xlabel(r"x [$\sigma_x$]")
 ax[1].set_xlabel(r"y [$\sigma_y$]")
@@ -155,19 +240,63 @@ ax[1].legend()
 fig.suptitle(r"CW off ($CWRx$)")
 
 # sim 2
-fig, ax = plt.subplots(1,2, figsize=(10,3))
+fig, ax = plt.subplots(1, 2, figsize=(10, 3))
 
-ax[0].plot(x_ele_cw_1[ x_ele_pos_idx]/sigma_x, ux_ele_cw_1[ x_ele_pos_idx]/sigma_px, "o", c="darkred", label=r"e- ($Rx>0$)")
-ax[0].plot(x_ele_cw_1[~x_ele_pos_idx]/sigma_x, ux_ele_cw_1[~x_ele_pos_idx]/sigma_px, "o", c="darkblue", label=r"e- ($Rx<0$)")
+ax[0].plot(
+    x_ele_cw_1[x_ele_pos_idx] / sigma_x,
+    ux_ele_cw_1[x_ele_pos_idx] / sigma_px,
+    "o",
+    c="darkred",
+    label=r"e- ($Rx>0$)",
+)
+ax[0].plot(
+    x_ele_cw_1[~x_ele_pos_idx] / sigma_x,
+    ux_ele_cw_1[~x_ele_pos_idx] / sigma_px,
+    "o",
+    c="darkblue",
+    label=r"e- ($Rx<0$)",
+)
 
-ax[0].plot(x_pos_cw_1[ x_pos_pos_idx]/sigma_x, ux_pos_cw_1[ x_pos_pos_idx]/sigma_px, "rx", label=r"e+ ($Rx>0$)")
-ax[0].plot(x_pos_cw_1[~x_pos_pos_idx]/sigma_x, ux_pos_cw_1[~x_pos_pos_idx]/sigma_px, "bx", label=r"e+ ($Rx<0$)")
+ax[0].plot(
+    x_pos_cw_1[x_pos_pos_idx] / sigma_x,
+    ux_pos_cw_1[x_pos_pos_idx] / sigma_px,
+    "rx",
+    label=r"e+ ($Rx>0$)",
+)
+ax[0].plot(
+    x_pos_cw_1[~x_pos_pos_idx] / sigma_x,
+    ux_pos_cw_1[~x_pos_pos_idx] / sigma_px,
+    "bx",
+    label=r"e+ ($Rx<0$)",
+)
 
-ax[1].plot(y_ele_cw_1[ x_ele_pos_idx]/sigma_y, uy_ele_cw_1[ x_ele_pos_idx]/sigma_py, "o", c="darkred",  label=r"e- ($Rx>0$)")
-ax[1].plot(y_ele_cw_1[~x_ele_pos_idx]/sigma_y, uy_ele_cw_1[~x_ele_pos_idx]/sigma_py, "o", c="darkblue", label=r"e- ($Rx<0$)")
+ax[1].plot(
+    y_ele_cw_1[x_ele_pos_idx] / sigma_y,
+    uy_ele_cw_1[x_ele_pos_idx] / sigma_py,
+    "o",
+    c="darkred",
+    label=r"e- ($Rx>0$)",
+)
+ax[1].plot(
+    y_ele_cw_1[~x_ele_pos_idx] / sigma_y,
+    uy_ele_cw_1[~x_ele_pos_idx] / sigma_py,
+    "o",
+    c="darkblue",
+    label=r"e- ($Rx<0$)",
+)
 
-ax[1].plot(y_pos_cw_1[ x_pos_pos_idx]/sigma_y, uy_pos_cw_1[ x_pos_pos_idx]/sigma_py, "rx", label=r"e+ ($Rx>0$)")
-ax[1].plot(y_pos_cw_1[~x_pos_pos_idx]/sigma_y, uy_pos_cw_1[~x_pos_pos_idx]/sigma_py, "bx", label=r"e+ ($Rx<0$)")
+ax[1].plot(
+    y_pos_cw_1[x_pos_pos_idx] / sigma_y,
+    uy_pos_cw_1[x_pos_pos_idx] / sigma_py,
+    "rx",
+    label=r"e+ ($Rx>0$)",
+)
+ax[1].plot(
+    y_pos_cw_1[~x_pos_pos_idx] / sigma_y,
+    uy_pos_cw_1[~x_pos_pos_idx] / sigma_py,
+    "bx",
+    label=r"e+ ($Rx<0$)",
+)
 
 ax[0].set_xlabel(r"x [$\sigma_x$]")
 ax[1].set_xlabel(r"y [$\sigma_y$]")
@@ -179,7 +308,8 @@ ax[1].legend()
 
 fig.suptitle(r"CW on ($Rx$)")
 
-def rot(x,z, phi):
+
+def rot(x, z, phi):
     """
     Anticlockwise rotation by phi
     To rotate clockwise use -phi
@@ -187,24 +317,25 @@ def rot(x,z, phi):
     """
     c = np.cos(phi)
     s = np.sin(phi)
-    x_rot =  c*x - s*z
-    z_rot =  s*x + c*z
+    x_rot = c * x - s * z
+    z_rot = s * x + c * z
     return x_rot, z_rot
+
 
 ############################################
 # apply inverse rotation on cw off dataset #
 ############################################
 
 # beam 1 rotate anticlockwise by phi
-x_ele_cw_0_init , z_ele_cw_0_init  = rot( x_ele_cw_0,  z_ele_cw_0, phi)
+x_ele_cw_0_init, z_ele_cw_0_init = rot(x_ele_cw_0, z_ele_cw_0, phi)
 ux_ele_cw_0_init, uz_ele_cw_0_init = rot(ux_ele_cw_0, uz_ele_cw_0, phi)
-y_ele_cw_0_init  = y_ele_cw_0
+y_ele_cw_0_init = y_ele_cw_0
 uy_ele_cw_0_init = uy_ele_cw_0
 
 # beam 2 rotate clockwise by phi
-x_pos_cw_0_init , z_pos_cw_0_init  = rot( x_pos_cw_0,  z_pos_cw_0, -phi)
+x_pos_cw_0_init, z_pos_cw_0_init = rot(x_pos_cw_0, z_pos_cw_0, -phi)
 ux_pos_cw_0_init, uz_pos_cw_0_init = rot(ux_pos_cw_0, uz_pos_cw_0, -phi)
-y_pos_cw_0_init  = y_pos_cw_0
+y_pos_cw_0_init = y_pos_cw_0
 uy_pos_cw_0_init = uy_pos_cw_0
 
 ##########################################################
@@ -212,52 +343,96 @@ uy_pos_cw_0_init = uy_pos_cw_0
 ##########################################################
 
 # beam 1 rotate anticlockwise by phi
-x_ele_cw_1_rot , z_ele_cw_1_rot  = rot( x_ele_cw_1,  z_ele_cw_1, phi)
+x_ele_cw_1_rot, z_ele_cw_1_rot = rot(x_ele_cw_1, z_ele_cw_1, phi)
 ux_ele_cw_1_rot, uz_ele_cw_1_rot = rot(ux_ele_cw_1, uz_ele_cw_1, phi)
 
 # beam 2 rotate clockwise by phi
-x_pos_cw_1_rot , z_pos_cw_1_rot  = rot( x_pos_cw_1,  z_pos_cw_1, -phi)
+x_pos_cw_1_rot, z_pos_cw_1_rot = rot(x_pos_cw_1, z_pos_cw_1, -phi)
 ux_pos_cw_1_rot, uz_pos_cw_1_rot = rot(ux_pos_cw_1, uz_pos_cw_1, -phi)
 
-acw_ele = -crabwaist_strength/np.tan(2*phi)
-acw_pos = -crabwaist_strength/np.tan(2*-phi)
+acw_ele = -crabwaist_strength / np.tan(2 * phi)
+acw_pos = -crabwaist_strength / np.tan(2 * -phi)
 
 # beam 1 apply inverse cw
-z_ele_cw_1_init  = z_ele_cw_1_rot
+z_ele_cw_1_init = z_ele_cw_1_rot
 uz_ele_cw_1_init = uz_ele_cw_1_rot
-x_ele_cw_1_init  = x_ele_cw_1_rot
+x_ele_cw_1_init = x_ele_cw_1_rot
 uy_ele_cw_1_init = uy_ele_cw_1
 
-ux_ele_cw_1_init = ux_ele_cw_1_rot - .5 * acw_ele * uy_ele_cw_1_init**2
-y_ele_cw_1_init  = y_ele_cw_1 + acw_ele * x_ele_cw_1_init * uy_ele_cw_1_init
+ux_ele_cw_1_init = ux_ele_cw_1_rot - 0.5 * acw_ele * uy_ele_cw_1_init**2
+y_ele_cw_1_init = y_ele_cw_1 + acw_ele * x_ele_cw_1_init * uy_ele_cw_1_init
 
 # beam 2 apply inverse cw
-z_pos_cw_1_init  = z_pos_cw_1_rot
+z_pos_cw_1_init = z_pos_cw_1_rot
 uz_pos_cw_1_init = uz_pos_cw_1_rot
-x_pos_cw_1_init  = x_pos_cw_1_rot
+x_pos_cw_1_init = x_pos_cw_1_rot
 uy_pos_cw_1_init = uy_pos_cw_1
 
-ux_pos_cw_1_init = ux_pos_cw_1_rot - .5 * acw_pos * uy_pos_cw_1_init**2
-y_pos_cw_1_init  = y_pos_cw_1 + acw_pos * x_pos_cw_1_init * uy_pos_cw_1_init
+ux_pos_cw_1_init = ux_pos_cw_1_rot - 0.5 * acw_pos * uy_pos_cw_1_init**2
+y_pos_cw_1_init = y_pos_cw_1 + acw_pos * x_pos_cw_1_init * uy_pos_cw_1_init
 
 ####################
 # plot phase space #
 ####################
 
 # sim 2 backtransformed
-fig, ax = plt.subplots(1,2, figsize=(10,3))
+fig, ax = plt.subplots(1, 2, figsize=(10, 3))
 
-ax[0].plot(x_ele_cw_1_init[ x_ele_pos_idx]/sigma_x, ux_ele_cw_1_init[ x_ele_pos_idx]/sigma_px, "o", c="darkred", label=r"e- ($Rx>0$)")
-ax[0].plot(x_ele_cw_1_init[~x_ele_pos_idx]/sigma_x, ux_ele_cw_1_init[~x_ele_pos_idx]/sigma_px, "o", c="darkblue", label=r"e- ($Rx<0$)")
+ax[0].plot(
+    x_ele_cw_1_init[x_ele_pos_idx] / sigma_x,
+    ux_ele_cw_1_init[x_ele_pos_idx] / sigma_px,
+    "o",
+    c="darkred",
+    label=r"e- ($Rx>0$)",
+)
+ax[0].plot(
+    x_ele_cw_1_init[~x_ele_pos_idx] / sigma_x,
+    ux_ele_cw_1_init[~x_ele_pos_idx] / sigma_px,
+    "o",
+    c="darkblue",
+    label=r"e- ($Rx<0$)",
+)
 
-ax[0].plot(x_pos_cw_1_init[ x_pos_pos_idx]/sigma_x, ux_pos_cw_1_init[ x_pos_pos_idx]/sigma_px, "rx", label=r"e+ ($Rx>0$)")
-ax[0].plot(x_pos_cw_1_init[~x_pos_pos_idx]/sigma_x, ux_pos_cw_1_init[~x_pos_pos_idx]/sigma_px, "bx", label=r"e+ ($Rx<0$)")
+ax[0].plot(
+    x_pos_cw_1_init[x_pos_pos_idx] / sigma_x,
+    ux_pos_cw_1_init[x_pos_pos_idx] / sigma_px,
+    "rx",
+    label=r"e+ ($Rx>0$)",
+)
+ax[0].plot(
+    x_pos_cw_1_init[~x_pos_pos_idx] / sigma_x,
+    ux_pos_cw_1_init[~x_pos_pos_idx] / sigma_px,
+    "bx",
+    label=r"e+ ($Rx<0$)",
+)
 
-ax[1].plot(y_ele_cw_1_init[ x_ele_pos_idx]/sigma_y, uy_ele_cw_1_init[ x_ele_pos_idx]/sigma_py, "o", c="darkred",  label=r"e- ($Rx>0$)")
-ax[1].plot(y_ele_cw_1_init[~x_ele_pos_idx]/sigma_y, uy_ele_cw_1_init[~x_ele_pos_idx]/sigma_py, "o", c="darkblue", label=r"e- ($Rx<0$)")
+ax[1].plot(
+    y_ele_cw_1_init[x_ele_pos_idx] / sigma_y,
+    uy_ele_cw_1_init[x_ele_pos_idx] / sigma_py,
+    "o",
+    c="darkred",
+    label=r"e- ($Rx>0$)",
+)
+ax[1].plot(
+    y_ele_cw_1_init[~x_ele_pos_idx] / sigma_y,
+    uy_ele_cw_1_init[~x_ele_pos_idx] / sigma_py,
+    "o",
+    c="darkblue",
+    label=r"e- ($Rx<0$)",
+)
 
-ax[1].plot(y_pos_cw_1_init[ x_pos_pos_idx]/sigma_y, uy_pos_cw_1_init[ x_pos_pos_idx]/sigma_py, "rx", label=r"e+ ($Rx>0$)")
-ax[1].plot(y_pos_cw_1_init[~x_pos_pos_idx]/sigma_y, uy_pos_cw_1_init[~x_pos_pos_idx]/sigma_py, "bx", label=r"e+ ($Rx<0$)")
+ax[1].plot(
+    y_pos_cw_1_init[x_pos_pos_idx] / sigma_y,
+    uy_pos_cw_1_init[x_pos_pos_idx] / sigma_py,
+    "rx",
+    label=r"e+ ($Rx>0$)",
+)
+ax[1].plot(
+    y_pos_cw_1_init[~x_pos_pos_idx] / sigma_y,
+    uy_pos_cw_1_init[~x_pos_pos_idx] / sigma_py,
+    "bx",
+    label=r"e+ ($Rx<0$)",
+)
 
 ax[0].set_xlabel(r"x [$\sigma_x$]")
 ax[1].set_xlabel(r"y [$\sigma_y$]")
@@ -274,17 +449,21 @@ fig.suptitle(r"CW on backtransformed ($CW^{-1}R^{-1}x$)")
 #########
 
 # these are trivial
-assert np.all(x_ele_cw_1_init==x_ele_cw_0_init)
-assert np.all(x_pos_cw_1_init==x_pos_cw_0_init)
+assert np.all(x_ele_cw_1_init == x_ele_cw_0_init)
+assert np.all(x_pos_cw_1_init == x_pos_cw_0_init)
 
-assert np.all(uy_ele_cw_1_init==uy_ele_cw_0_init)
-assert np.all(uy_pos_cw_1_init==uy_pos_cw_0_init)
+assert np.all(uy_ele_cw_1_init == uy_ele_cw_0_init)
+assert np.all(uy_pos_cw_1_init == uy_pos_cw_0_init)
 
-assert np.all(z_ele_cw_1_init==z_ele_cw_0_init)
-assert np.all(z_pos_cw_1_init==z_pos_cw_0_init)
+assert np.all(z_ele_cw_1_init == z_ele_cw_0_init)
+assert np.all(z_pos_cw_1_init == z_pos_cw_0_init)
 
-assert np.allclose(uz_ele_cw_1_init/gamma, uz_ele_cw_0_init/gamma, rtol=1e-14, atol=1e-14)
-assert np.allclose(uz_pos_cw_1_init/gamma, uz_pos_cw_0_init/gamma, rtol=1e-14, atol=1e-14)
+assert np.allclose(
+    uz_ele_cw_1_init / gamma, uz_ele_cw_0_init / gamma, rtol=1e-14, atol=1e-14
+)
+assert np.allclose(
+    uz_pos_cw_1_init / gamma, uz_pos_cw_0_init / gamma, rtol=1e-14, atol=1e-14
+)
 
 # cw was applied on these
 assert np.allclose(ux_ele_cw_1_init, ux_ele_cw_0_init, rtol=1e-6, atol=1e-12)
