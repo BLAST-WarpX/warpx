e03c8fa6ea6016fdb8be3f80a877e420fab3a697
Olga Shapoval <oshapoval@lbl.gov>
2026-02-12 18:59:37 -0800

    Resolved conflicts from cherry-pick (part 5)

--- Source/Particles/PhysicalParticleContainer.cpp
+++ Source/Particles/PhysicalParticleContainer.cpp
@@ -474,23 +474,23 @@ PhysicalParticleContainer::Evolve (ablastr::fields::MultiFabRegister& fields,
         has_rho &&
         !skip_deposition &&
         !do_not_deposit &&
-        position_push_type == PositionPushType::Full
-    );
-    bool const split_particles = (
-        do_splitting &&
-        subcycling_half == SubcyclingHalf::None &&
-        position_push_type == PositionPushType::Full
+        (momentum_push_type == MomentumPushType::Full || momentum_push_type == MomentumPushType::SecondHalf)
     );
     bool const deposit_charge_new = (
         has_rho &&
         !skip_deposition &&
         !do_not_deposit &&
-        (position_push_type == PositionPushType::Full || position_push_type == PositionPushType::SecondHalf)
+        (momentum_push_type == MomentumPushType::Full || momentum_push_type == MomentumPushType::SecondHalf)
     );
     bool const deposit_current = (
         !skip_deposition &&
         !do_not_deposit &&
         !(implicit_options && implicit_options->evolve_suborbit_particles_only) &&
+        (momentum_push_type == MomentumPushType::Full || momentum_push_type == MomentumPushType::SecondHalf)
+    );
+    bool const split_particles = (
+        do_splitting &&
+        (subcycling_half == SubcyclingHalf::None || subcycling_half == SubcyclingHalf::SecondHalf) &&
         (momentum_push_type == MomentumPushType::Full || momentum_push_type == MomentumPushType::SecondHalf)
     );
 
