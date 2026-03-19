6b990e798c72ef658d3f677aebd9add927ac744a
Davide Terzani <23354403+titoiride@users.noreply.github.com>
2026-03-19 10:06:56 -0700

    Update installation files to stable modules

--- Tools/machines/andes-olcf/install_andes_analysis.sh
+++ Tools/machines/andes-olcf/install_andes_analysis.sh
@@ -21,27 +21,62 @@ then
     return
 fi
 
-# Install necessary dependencies for openpmd-api, as ADIOS2 version available on andes is too old
+# Install necessary dependencies for openpmd-api, as version available on andes is too old
 # tmpfs build directory: avoids issues often seen with $HOME and is faster
 build_dir=$(mktemp -d)
-echo "Building c-blosc"
-if [ -d ${SW_DIR}/c-blosc ]
+
+# HDF5 (parallel)
+echo "Building HDF5 (parallel)"
+HDF5_VERSION="1.14.3"
+HDF5_TAG="hdf5_${HDF5_VERSION//./_}"
+
+if [ -d ${SW_DIR}/hdf5 ]
 then
-  cd ${SW_DIR}/c-blosc
+  cd ${SW_DIR}/hdf5
   git fetch --prune
-  git checkout v1.21.1
+  git checkout ${HDF5_TAG}
   cd -
 else
-  git clone -b v1.21.1 https://github.com/Blosc/c-blosc ${SW_DIR}/c-blosc
+  git clone -b ${HDF5_TAG} https://github.com/HDFGroup/hdf5.git ${SW_DIR}/hdf5
 fi
-if [ -d ${build_dir}/c-blosc-build ]
+
+if [ -d ${build_dir}/hdf5-build ]
 then
-  rm -rf ${build_dir}/c-blosc-build
+  rm -rf ${build_dir}/hdf5-build
 fi
-CC=mpicc CXX=mpicxx cmake -S  ${SW_DIR}/c-blosc -B ${build_dir}/c-blosc-build -DBUILD_TESTS=ON -DBUILD_BENCHMARKS=OFF -DDEACTIVATE_AVX2=OFF -DCMAKE_INSTALL_PREFIX=${SW_DIR}/c-blosc-1.21.1
-cmake --build  ${build_dir}/c-blosc-build --target install --parallel 16
-rm -rf  ${build_dir}/c-blosc-build
-echo "c-blosc built"
+
+CC=mpicc CXX=mpicxx cmake \
+  -S ${SW_DIR}/hdf5 \
+  -B ${build_dir}/hdf5-build --fresh \
+  -DHDF5_ENABLE_PARALLEL=ON \
+  -DHDF5_BUILD_FORTRAN=OFF \
+  -DHDF5_BUILD_JAVA=OFF \
+  -DHDF5_BUILD_EXAMPLES=OFF \
+  -DBUILD_TESTING=OFF \
+  -DCMAKE_INSTALL_PREFIX=${SW_DIR}/hdf5-${HDF5_VERSION}
+
+cmake --build ${build_dir}/hdf5-build --target install --parallel 16
+rm -rf ${build_dir}/hdf5-build
+echo "HDF5 (parallel) built"
+
+echo "Building c-blosc 2"
+if [ -d ${SW_DIR}/c-blosc2 ]
+then
+  cd ${SW_DIR}/c-blosc2
+  git fetch --prune
+  git checkout v2.23.1
+  cd -
+else
+  git clone -b v2.23.1 https://github.com/Blosc/c-blosc2 ${SW_DIR}/c-blosc2
+fi
+if [ -d ${build_dir}/c-blosc2-build ]
+then
+  rm -rf ${build_dir}/c-blosc2-build
+fi
+CC=mpicc CXX=mpicxx cmake -S  ${SW_DIR}/c-blosc2 -B ${build_dir}/c-blosc2-build --fresh -DBUILD_TESTS=ON -DBUILD_BENCHMARKS=OFF -DDEACTIVATE_AVX2=OFF -DCMAKE_INSTALL_PREFIX=${SW_DIR}/c-blosc2-2.23.1
+cmake --build  ${build_dir}/c-blosc2-build --target install --parallel 16
+rm -rf  ${build_dir}/c-blosc2-build
+echo "c-blosc2 built"
 
 # ADIOS2
 echo "Building adios2"
@@ -58,7 +93,7 @@ if [ -d ${build_dir}/adios2-build ]
 then
   rm -rf ${build_dir}/adios2-build
 fi
-CC=mpicc CXX=mpicxx cmake -S ${SW_DIR}/adios2 -B ${build_dir}/adios2-build -DADIOS2_USE_Blosc=ON -DADIOS2_USE_Fortran=OFF -DADIOS2_USE_Python=OFF -DADIOS2_USE_ZeroMQ=OFF -DCMAKE_INSTALL_PREFIX=${SW_DIR}/adios2-2.10.2
+CC=mpicc CXX=mpicxx cmake -S ${SW_DIR}/adios2 -B ${build_dir}/adios2-build --fresh -DADIOS2_USE_Blosc2=ON -DADIOS2_USE_Fortran=OFF -DADIOS2_USE_Python=OFF -DADIOS2_USE_ZeroMQ=OFF -DCMAKE_INSTALL_PREFIX=${SW_DIR}/adios2-2.10.2
 cmake --build ${build_dir}/adios2-build --target install --parallel 16
 rm -rf ${build_dir}/adios2-build
 echo "adios2 built"
@@ -91,15 +126,15 @@ python -m pip install --upgrade numpy
 python -m pip install --upgrade scipy
 
 # Extra packages for python ######################################################################
-# 
+#
 python -m pip install --upgrade pandas
-CC="mpicc -shared" CXX="mpicxx -shared" python -m pip install --upgrade mpi4py --no-cache-dir --no-build-isolation --no-binary mpi4py -v
-# This installs openpmd-api with MPI support
-CC="mpicc" CXX="mpicxx" openPMD_USE_MPI=ON openPMD_USE_ADIOS2=ON openPMD_USE_HDF5=ON python -m pip install openpmd-api --force-reinstall --no-binary openpmd-api -vv
+CC=mpicc CXX=mpicxx python -m pip install --upgrade mpi4py --no-cache-dir --no-build-isolation --no-binary mpi4py
+CC=mpicc CXX=mpicxx openPMD_USE_MPI=ON openPMD_USE_ADIOS2=ON openPMD_USE_HDF5=ON python -m pip install openpmd-api --no-binary openpmd-api -vv
 python -m pip install --upgrade openpmd_viewer
 python -m pip install --upgrade matplotlib
 python -m pip install --upgrade yt
 python -m pip install --upgrade dask
 python -m pip install --upgrade pyarrow
 python -m pip install --upgrade ipython
+python -m pip install --upgrade hepunits
 python -m pip install --upgrade numba
