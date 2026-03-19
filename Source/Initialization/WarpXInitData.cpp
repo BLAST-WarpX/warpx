1f3a59999899ba0b3267e0ce38b754369cd901b4
lucafedeli88 <luca.fedeli@cea.fr>
2026-03-19 01:52:12 +0100

    change position of NOLINT

--- Source/Initialization/WarpXInitData.cpp
+++ Source/Initialization/WarpXInitData.cpp
@@ -110,13 +110,14 @@ namespace
                        injection_style.begin(),
                        ::tolower);
 
+        // NOLINT(bugprone-branch-clone)
         if (injection_style == "singleparticle") {
             nppc = 1;
         } else if (injection_style == "multipleparticles") {
             std::vector<int> multiple_particles_pos_x;
             utils::parser::getArrWithParser(pp_spec, "multiple_particles_pos_x", multiple_particles_pos_x);
             nppc = multiple_particles_pos_x.size();
-        } else if (injection_style == "gaussian_beam") { // NOLINT(bugprone-branch-clone)
+        } else if (injection_style == "gaussian_beam") {
             // TODO: hard to estimate well
             // Possible way: take the npart parameter, normalize by rms scale to nppc via cell size on level 0.
             nppc = 1;
