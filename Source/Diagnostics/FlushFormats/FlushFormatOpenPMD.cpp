fa9fc97432b4be158e7960d1751763a03b07ca6d
Luca Fedeli <luca.fedeli@cea.fr>
2026-03-25 10:43:30 +0100

    implement JustinRayAngus's suggestion

--- Source/Diagnostics/FlushFormats/FlushFormatOpenPMD.cpp
+++ Source/Diagnostics/FlushFormats/FlushFormatOpenPMD.cpp
@@ -178,7 +178,7 @@ FlushFormatOpenPMD::WriteToFile (
     m_OpenPMDPlotWriter->WriteOpenPMDParticles(
         particle_diags, static_cast<amrex::Real>(time), use_pinned_pc, isBTD, isLastBTDFlush);
 
-    if (isBTD  && (bufferID % m_NumAggBTDBufferToFlush == 0) ){
+    if (isBTD  && (bufferID % m_NumAggBTDBufferToFlush == 0) ) {
         m_OpenPMDPlotWriter->FlushBTDToDisk();
     }
 
