e5eb91b14328901f1293ba163afbb435dcfe05fa
bowen zhu <2062141317@qq.com>
2026-04-06 08:21:17 +0800

    FieldSolver: use c_light (ceff) in Silver-Mueller ABC boundary
    Replace PhysConst::c with WarpX::c_light in ApplySilverMuellerBoundary
    so that the absorbing boundary coefficients are consistent with the
    reduced speed of light model (warpx.c_light_factor > 1).

--- Source/FieldSolver/FiniteDifferenceSolver/ApplySilverMuellerBoundary.cpp
+++ Source/FieldSolver/FiniteDifferenceSolver/ApplySilverMuellerBoundary.cpp
@@ -12,6 +12,7 @@
 #include "Utils/TextMsg.H"
 #include "Utils/WarpXAlgorithmSelection.H"
 #include "Utils/WarpXConst.H"
+#include "WarpX.H"
 
 #include <AMReX.H>
 #include <AMReX_Array4.H>
@@ -59,15 +60,15 @@ void FiniteDifferenceSolver::ApplySilverMuellerBoundary (
 
 #if defined(WARPX_DIM_RZ) || defined(WARPX_DIM_RCYLINDER) || defined(WARPX_DIM_RSPHERE)
     // Calculate relevant coefficients
-    amrex::Real const cdt = PhysConst::c*dt;
+    amrex::Real const cdt = WarpX::c_light*dt;
     amrex::Real const cdt_over_dr = cdt*m_h_stencil_coefs_r[0];
     amrex::Real const coef1_r = (1._rt - cdt_over_dr)/(1._rt + cdt_over_dr);
-    amrex::Real const coef2_r = 2._rt*cdt_over_dr/(1._rt + cdt_over_dr) / PhysConst::c;
-    amrex::Real const coef3_r = cdt/(1._rt + cdt_over_dr) / PhysConst::c;
+    amrex::Real const coef2_r = 2._rt*cdt_over_dr/(1._rt + cdt_over_dr) / WarpX::c_light;
+    amrex::Real const coef3_r = cdt/(1._rt + cdt_over_dr) / WarpX::c_light;
 #if defined(WARPX_DIM_RZ)
     amrex::Real const cdt_over_dz = cdt*m_h_stencil_coefs_z[0];
     amrex::Real const coef1_z = (1._rt - cdt_over_dz)/(1._rt + cdt_over_dz);
-    amrex::Real const coef2_z = 2._rt*cdt_over_dz/(1._rt + cdt_over_dz) / PhysConst::c;
+    amrex::Real const coef2_z = 2._rt*cdt_over_dz/(1._rt + cdt_over_dz) / WarpX::c_light;
 #endif
 
 #if !defined(WARPX_DIM_RSPHERE)
@@ -217,18 +218,18 @@ void FiniteDifferenceSolver::ApplySilverMuellerBoundary (
 
     // Calculate relevant coefficients
 #if (defined WARPX_DIM_3D || WARPX_DIM_XZ)
-    amrex::Real const cdt_over_dx = PhysConst::c*dt*m_h_stencil_coefs_x[0];
+    amrex::Real const cdt_over_dx = WarpX::c_light*dt*m_h_stencil_coefs_x[0];
     amrex::Real const coef1_x = (1._rt - cdt_over_dx)/(1._rt + cdt_over_dx);
-    amrex::Real const coef2_x = 2._rt*cdt_over_dx/(1._rt + cdt_over_dx) / PhysConst::c;
+    amrex::Real const coef2_x = 2._rt*cdt_over_dx/(1._rt + cdt_over_dx) / WarpX::c_light;
 #endif
 #ifdef WARPX_DIM_3D
-    amrex::Real const cdt_over_dy = PhysConst::c*dt*m_h_stencil_coefs_y[0];
+    amrex::Real const cdt_over_dy = WarpX::c_light*dt*m_h_stencil_coefs_y[0];
     amrex::Real const coef1_y = (1._rt - cdt_over_dy)/(1._rt + cdt_over_dy);
-    amrex::Real const coef2_y = 2._rt*cdt_over_dy/(1._rt + cdt_over_dy) / PhysConst::c;
+    amrex::Real const coef2_y = 2._rt*cdt_over_dy/(1._rt + cdt_over_dy) / WarpX::c_light;
 #endif
-    amrex::Real const cdt_over_dz = PhysConst::c*dt*m_h_stencil_coefs_z[0];
+    amrex::Real const cdt_over_dz = WarpX::c_light*dt*m_h_stencil_coefs_z[0];
     amrex::Real const coef1_z = (1._rt - cdt_over_dz)/(1._rt + cdt_over_dz);
-    amrex::Real const coef2_z = 2._rt*cdt_over_dz/(1._rt + cdt_over_dz) / PhysConst::c;
+    amrex::Real const coef2_z = 2._rt*cdt_over_dz/(1._rt + cdt_over_dz) / WarpX::c_light;
 
 #if (defined WARPX_DIM_3D || WARPX_DIM_XZ)
     bool const apply_lo_x = (field_boundary_lo[0] == FieldBoundaryType::Absorbing_SilverMueller);
