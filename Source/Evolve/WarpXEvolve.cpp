b036fea3f443c4b6c252980c904abf9e80131460
Olga Shapoval <oshapoval@lbl.gov>
2026-04-08 10:57:59 -0700

    Add checks for MR subcycling requirements

--- Source/Evolve/WarpXEvolve.cpp
+++ Source/Evolve/WarpXEvolve.cpp
@@ -56,6 +56,7 @@
 #include <array>
 #include <memory>
 #include <ostream>
+#include <string>
 #include <vector>
 
 using namespace amrex;
@@ -1064,9 +1065,32 @@ WarpX::OneStep_sub1 (Real cur_time)
 
     // TODO: we could save some charge depositions
 
-    WARPX_ALWAYS_ASSERT_WITH_MESSAGE(finest_level == 1, "Must have exactly two levels");
+    // check that MR has exactly two levels and a refinement ratio of 2 in all directions when subcycling is used
     const int fine_lev = 1;
     const int coarse_lev = 0;
+    const amrex::IntVect& ref_ratio = this->refRatio(coarse_lev);
+    bool ref_ratio_is_uniform_two = true;
+    for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
+        if (ref_ratio[idim] != 2) {
+            ref_ratio_is_uniform_two = false;
+            break;
+        }
+    }
+    if (finest_level != 1 || !ref_ratio_is_uniform_two) {
+        std::ostringstream msg;
+        msg << "MR with subcycling algorithm requires exactly two levels of MR "
+            << "and refinement ratio of2 in all directions. "
+            << "Found: finest_level = " << finest_level
+            << ", refRatio(" << coarse_lev << ") = (";
+
+        for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
+            msg << ref_ratio[idim];
+            if (idim < AMREX_SPACEDIM - 1) msg << ", ";
+        }
+        msg << ")";
+
+        WARPX_ABORT_WITH_MESSAGE(msg.str().c_str());
+    }
 
     using warpx::fields::FieldType;
 
