fc71a3d45246cf8dead04a53bdc1e8bb692081cc
Justin Angus <angus1@llnl.gov>
2026-05-12 16:38:58 -0700

    update solver tol for analysis.

--- Examples/Tests/implicit/analysis_pinch.py
+++ Examples/Tests/implicit/analysis_pinch.py
@@ -61,13 +61,13 @@ assert max_rel_net_energy < rel_net_energy_tol
 # check that the number of gmres iterations is below tolerance
 print(f"gmres iters per newton: {total_gmres_iters / total_newton_iters}")
 print(f"gmres iters tolerance: {gmres_iters_tol}")
-assert total_gmres_iters / total_newton_iters < gmres_iters_tol
+assert total_gmres_iters / total_newton_iters <= gmres_iters_tol
 
 # check that the number of newton iterations is below tolerance
 newton_iters_tol = 5
 print(f"newton iters per time step: {total_newton_iters / num_steps}")
 print(f"newton iters tolerance: {newton_iters_tol}")
-assert total_newton_iters / num_steps < newton_iters_tol
+assert total_newton_iters / num_steps <= newton_iters_tol
 
 # check for machine precision conservation of charge density
 n0 = 1.0e23
