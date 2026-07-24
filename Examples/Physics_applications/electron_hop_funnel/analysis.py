3bac1ebbd2588f53ac880acb721e3d7e41bc11cb
Marcus Pearlman <marcuspearlman@boisestate.edu>
2026-07-24 12:49:23 -0600

    add basic picmi test for spatial dielectric electrostatic solver
    This example is the beginings of an electron hop funnel model. Electron current emits from the bottom, electrons are pulled up by the electric field created by the hop electrod, they "hop" along the dielectric funnel via secondary electron emission, and exit the funnel and are pulled to the anode. With a sufficient hop electrod voltage, the dielectric surface charges to the point where the secondary electron yield is 1.0 and all electron current transmits through the funnel.
    
    The hop funnel provides a simple-ish example to test surface charging on cut-cell dielectric boundaries. Current will not transmit with improper handling of surface charge.

---
+++ Examples/Physics_applications/electron_hop_funnel/analysis.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import matplotlib
+
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import numpy as np
+from openpmd_viewer import OpenPMDTimeSeries
+
+filename = './diags/diag1/'
+
+ts = OpenPMDTimeSeries(filename)
+iterations = ts.iterations
+phi,info = ts.get_field("phi", iteration=iterations[-1], plot=False)
+
+fig, ax = plt.subplots(figsize=(8, 6)) 
+im = ax.imshow(phi, origin='lower', aspect='auto', cmap='hsv')
+fig.colorbar(im, ax=ax)
+fig.savefig('./phi.png',dpi=300,bbox_inches="tight")
+
